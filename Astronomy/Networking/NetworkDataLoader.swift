5df4dcbddc7857bcd250862978fbec8acdb0868b
Conner Alegre <cga0@pm.me>
2018-09-17 14:03:21 -0600

    fix typo

--- Astronomy/Networking/NetworkDataLoader.swift
+++ Astronomy/Networking/NetworkDataLoader.swift
@@ -16,7 +16,7 @@ protocol NetworkDataLoader {
 extension URLSession: NetworkDataLoader {
     
     func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
-        dataTask(with: request) { (daa, _, error) in
+        dataTask(with: request) { (data, _, error) in
             completion(data, error)
         }.resume()
     }
