7d0fba7253a852ae26a1d16cc6748bbaf0b68ad8
Conner Alegre <cga0@pm.me>
2018-09-17 14:24:30 -0600

    implement MockDataLoader for tests

--- AstronomyTests/MarsRoverClientTests.swift
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -7,6 +7,29 @@
 //
 
 import XCTest
+@testable import Astronomy
+
+class MockDataLoader: NetworkDataLoader {
+    init(data: Data?, error: Error?) {
+        self.data = data
+        self.error = error
+    }
+    
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        self.request = request
+        completion(self.data, self.error)
+    }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        self.url = url
+        completion(self.data, self.error)
+    }
+    
+    let data: Data?
+    let error: Error?
+    private(set) var request: URLRequest? = nil
+    private(set) var url: URL? = nil
+}
 
 class MarsRoverClientTests: XCTestCase {
     func testFetchMarsRover() {
