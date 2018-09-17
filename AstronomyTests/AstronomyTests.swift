79dcc6c519a7794132b804d94c9236359bdee4eb
William Bundy <williambundy@icloud.com>
2018-09-17 14:55:30 -0700

    Created a MockLoader in test project

--- AstronomyTests/AstronomyTests.swift
+++ AstronomyTests/AstronomyTests.swift
@@ -7,8 +7,38 @@
 //
 
 import XCTest
+@testable import Astronomy
+
+class MockLoader:NetworkDataLoader
+{
+	var lastURL:URL?
+	var lastRequest:URLRequest?
+
+	var data:Data?
+	var error:Error?
+
+	init(data:Data?, error:Error?)
+	{
+		self.data = data
+		self.error = error
+	}
+
+
+	func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void)
+	{
+		self.lastURL = url
+		completion(data, error)
+	}
+
+	func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void)
+	{
+		self.lastRequest = request
+		completion(data, error)
+	}
+}
+
 
 class AstronomyTests: XCTestCase
 {
-
+	
 }
