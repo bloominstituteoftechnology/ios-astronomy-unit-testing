4f9a04143dc16e24b639a04ab0eaaabaf0bd6721
William Bundy <williambundy@icloud.com>
2018-09-17 14:53:21 -0700

    Added NetworkDataLoader

---
+++ AstronomyTests/AstronomyTests.swift
@@ -0,0 +1,14 @@
+//
+//  AstronomyTests.swift
+//  AstronomyTests
+//
+//  Created by William Bundy on 9/17/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class AstronomyTests: XCTestCase
+{
+
+}
