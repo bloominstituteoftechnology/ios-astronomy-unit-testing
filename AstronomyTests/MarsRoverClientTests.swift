ef1460fef6aa2b56cb2fef0e5aa2f2096b302df3
Conner Alegre <cga0@pm.me>
2018-09-17 14:20:31 -0600

    add testFetchMarsRover & testFetchPhotos empty to tests

---
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -0,0 +1,19 @@
+//
+//  AstronomyTests.swift
+//  AstronomyTests
+//
+//  Created by Conner on 9/17/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class MarsRoverClientTests: XCTestCase {
+    func testFetchMarsRover() {
+        
+    }
+    
+    func testFetchPhotos() {
+        
+    }
+}
