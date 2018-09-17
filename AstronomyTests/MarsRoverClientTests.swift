a83dca31ebcaf3689849958a3486dd00907c5505
Lisa Sampson <lisasampson0918@gmail.com>
2018-09-17 18:05:00 -0400

    Set up MockLoader for testing

--- AstronomyTests/MarsRoverClientTests.swift
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -7,9 +7,63 @@
 //
 
 import XCTest
+@testable import Astronomy
+
+class MockLoader: NetworkDataLoader {
+    
+    init(data: Data? = nil, error: Error? = nil) {
+        self.data = data
+        self.error = error
+    }
+    
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        self.request = request
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            completion(self.data, self.error)
+        }
+    }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        self.url = url
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            completion(self.data, self.error)
+        }
+    }
+    
+    let data: Data?
+    let error: Error?
+    private(set) var request: URLRequest? = nil
+    private(set) var url: URL? = nil
+}
 
 class MarsRoverClientTests: XCTestCase {
     
+    func testFetchMarsRover() {
+        
+    }
     
+    func testFetchPhotos() {
+        
+    }
     
+}
+
+func urlComponents(_ components1: URLComponents, equalTo components2: URLComponents) -> Bool {
+    var scratch1 = components1
+    var scratch2 = components2
+    
+    scratch1.queryItems = []
+    scratch2.queryItems = []
+    if scratch1 != scratch2 {
+        return false
+    }
+    
+    //Compare Query items
+    if let queryItems1 = components1.queryItems,
+        let queryItems2 = components2.queryItems {
+        if Set(queryItems1) != Set(queryItems2) {
+            return false
+        }
+    }
+    return true
 }
