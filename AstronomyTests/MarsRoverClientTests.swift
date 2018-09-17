2626572e3aeacd17d65357fb8e3ba9c568c1c5ab
Lisa Sampson <lisasampson0918@gmail.com>
2018-09-17 17:45:48 -0400

    refactor MarsRoverClient to allow for dependency injection

---
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -0,0 +1,15 @@
+//
+//  MarsRoverClientTests.swift
+//  AstronomyTests
+//
+//  Created by Lisa Sampson on 9/17/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class MarsRoverClientTests: XCTestCase {
+    
+    
+    
+}
