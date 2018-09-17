01c1bfe2d242cd9f91610a936e526bcd75f2d863
Conner Alegre <cga0@pm.me>
2018-09-17 14:03:03 -0600

    implement loadData variants of NetworkDataLoader protocol into URLSession as extension

--- Astronomy/Networking/NetworkDataLoader.swift
+++ Astronomy/Networking/NetworkDataLoader.swift
@@ -12,3 +12,18 @@ protocol NetworkDataLoader {
     func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void)
     func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void)
 }
+
+extension URLSession: NetworkDataLoader {
+    
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        dataTask(with: request) { (daa, _, error) in
+            completion(data, error)
+        }.resume()
+    }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        dataTask(with: url) { (data, _, error) in
+            completion(data, error)
+        }.resume()
+    }
+}
