f99eaff8efd5915e93212c347db280ff61912573
Sergey Osipyan <osipyansergey@gmail.com>
2019-02-11 13:37:23 -0800

    Part 1 done

---
+++ AstronomyTests/MarsRoverClientTest.swift
@@ -0,0 +1,15 @@
+//
+//  MarsRoverClientTest.swift
+//  AstronomyTests
+//
+//  Created by Sergey Osipyan on 2/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class MarsRoverClientTest: XCTestCase {
+
+   
+
+}
