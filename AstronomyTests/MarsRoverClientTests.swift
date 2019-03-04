88d6624f592b946849317f898a89a69311d98842
Nelson Gonzalez <nelglezfl@gmail.com>
2019-03-04 15:46:36 -0500

    Test fetch photos complete

--- AstronomyTests/MarsRoverClientTests.swift
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -11,7 +11,7 @@ import XCTest
 
 class MarsRoverClientTests: XCTestCase {
 
-
+    var photoFetchResults: [MarsPhotoReference] = []
     //You should at least test fetchMarsRover() and fetchPhotos()
     
     
@@ -22,15 +22,34 @@ class MarsRoverClientTests: XCTestCase {
         //1.
         let marsRoverClient = MarsRoverClient(networkLoader: mockLoader)
        
-        //call method we want to search
+        //call method we want
         marsRoverClient.fetchMarsRover(named: "Curiosity") { (rover, error) in
             XCTAssertTrue(rover!.maxSol>0)
              expectations.fulfill()
         }
         
          waitForExpectations(timeout: 5, handler: nil)
+    }
     
+    func testFetchPhotosValidDataAndNoErrorMessage() {
+       let expectations = expectation(description: "Results should have valid data and no errors")
         
+        let mockLoader = MockLoader(data: validSol1JSON, error: nil)
+        
+        let marsRoverClient = MarsRoverClient(networkLoader: mockLoader)
+        
+        let decoder = MarsPhotoReference.jsonDecoder
+        let rover = (try! decoder.decode([String: MarsRover].self, from: validRoverJSON))["photoManifest"]!
+        
+        
+        marsRoverClient.fetchPhotos(from: rover, onSol: 1) { (photoRef, error) in
+            guard let roverPhotos = photoRef else {return}
+            XCTAssertTrue(roverPhotos.count > 0)
+            XCTAssertTrue(roverPhotos.count == 16)
+            expectations.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5)
         
     }
     
