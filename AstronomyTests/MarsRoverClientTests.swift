ccbd3bb1d8fc0709298a2ccbe15edb3ef92e8a40
Nelson Gonzalez <nelglezfl@gmail.com>
2019-03-04 14:53:53 -0500

    Add MockLoader Struct

---
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -0,0 +1,15 @@
+//
+//  MarsRoverClientTests.swift
+//  AstronomyTests
+//
+//  Created by Nelson Gonzalez on 3/4/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class MarsRoverClientTests: XCTestCase {
+
+    //You should at least test fetchMarsRover() and fetchPhotos()
+
+}
