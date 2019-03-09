59bb74b08eee433ea95bce0372e47b88128fccb4
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-03-08 21:19:10 -0500

    add load data methods

--- Astronomy/NetworkDataLoader.swift
+++ Astronomy/NetworkDataLoader.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+protocol NetworkDataLoader {
+    
+    func loadData(from request: URLRequest, completion: @escaping(Data?, Error?) -> Void)
+    
+    
+    func loadData(from url: URL, completion: @escaping(Data?, Error?) -> Void)
+}
+
+
+extension URLSession: NetworkDataLoader {
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        let task = self.dataTask(with: request) { (data, _, error) in
+            completion(data, error)
+        }
+        task.resume()
+    }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        let task = self.dataTask(with: url) { (data, _, error) in
+            completion(data, error)
+        }
+        task.resume()
+    }
+    
+    
+}
