55d1d7d2c4a4fb045bba7857a5e6f470ed2268ca
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-03-08 22:16:04 -0500

    delete given test class and create a new one

--- AstronomyTests/MarsRoverClientTests.swift
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -1,5 +1,5 @@
 //
-//  AstronomyTests.swift
+//  MarsRoverClientTests.swift
 //  AstronomyTests
 //
 //  Created by Julian A. Fordyce on 3/8/19.
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class AstronomyTests: XCTestCase {
+class MarsRoverClientTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
