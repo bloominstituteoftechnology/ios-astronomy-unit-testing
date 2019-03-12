8cb2455c2203135dbc1ef92a0465b5ca6ace701e
Paul Yi <yipaulc@gmail.com>
2019-03-11 20:37:04 -0700

    Refactor MarsRoverClient to improve testability
    Abstract the APIs needed from URLSession into a protocol. Make URLSession conform to protocol by using an extension. Make MarsRoverClient accept an object conforming to NetworkDataLoader in its initializer, instead of a URLSession instance.

--- Astronomy/Model/NetworkDataLoader.swift
+++ Astronomy/Model/NetworkDataLoader.swift
@@ -9,17 +9,21 @@
 import Foundation
 
 protocol NetworkDataLoader {
-    
     func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void)
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void)
 }
 
 extension URLSession: NetworkDataLoader {
+    
     func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
-        
         dataTask(with: request) { (data, _, error) in
             completion(data, error)
-        }.resume()
+            }.resume()
     }
     
-
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        dataTask(with: url) { data, _, error in
+            completion(data, error)
+            }.resume()
+    }
 }
