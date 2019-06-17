67b1dde9ee3085414221787b1434f47fe216596c
Michael Redig <mredig@gmail.com>
2019-06-17 14:57:44 -0500

    added networkdataloader protocol

---
+++ Astronomy/Networking/NetworkDataLoader.swift
@@ -0,0 +1,30 @@
+//
+//  NetworkDataLoader.swift
+//  Astronomy
+//
+//  Created by Michael Redig on 6/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkDataLoader {
+	func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void)
+	func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void)
+}
+
+extension URLSession: NetworkDataLoader {
+	func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+		let task = self.dataTask(with: request) { (data, _, error) in
+			completion(data, error)
+		}
+		task.resume()
+	}
+
+	func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+		let task = self.dataTask(with: url) { (data, _, error) in
+			completion(data, error)
+		}
+		task.resume()
+	}
+}
---
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -0,0 +1,13 @@
+//
+//  MarsRoverClientTests.swift
+//  MarsRoverClientTests
+//
+//  Created by Michael Redig on 6/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class MarsRoverClientTests: XCTestCase {
+
+}
