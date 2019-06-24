1bbe1020afde37cd7e4ba291a4f241ff1e5bee1f
Mitchell Budge <mitchellbudge@gmail.com>
2019-06-24 15:05:34 -0600

    Configure unit tests for fetching rovers and fetching photos
    Need to debug/refactor

--- AstronomyTests/MarsRoverClientTests.swift
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -12,11 +12,27 @@ import XCTest
 class MarsRoverClientTests: XCTestCase {
     
     func testFetchMarsRover() {
+        let mockLoader = MockLoader(data: validSol1JSON, response: nil, error: nil)
+        let marsRoverClient = MarsRoverClient(networkLoader: mockLoader)
+        let expectation = self.expectation(description: "A valid rover exists")
         
+        marsRoverClient.fetchMarsRover(named: "Curiosity") { (marsRover, error) in
+            XCTAssert(marsRover != nil)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5)
     }
     
     func testFetchPhotos() {
+        let mockLoader = MockLoader(data: validRoverJSON, response: nil, error: nil)
+        let marsRoverClient = MarsRoverClient(networkLoader: mockLoader)
+        let expectation = self.expectation(description: "A valid set of photos exists")
         
+        marsRoverClient.fetchPhotos(from: validRoverJSON, onSol: 0) { (photos, error) in
+            XCTAssert(photos =! nil)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5)
     }
 
     let validRoverJSON = """
