b36ec14dd664f23e2961118fd92c06ca41eb5a96
jayfenam <jonathanferrer95@gmail.com>
2019-06-24 16:25:28 -0400

    Verified that the app runs with the changes to the URLSessions

--- Astronomy/Networking/MarsRoverClient.swift
+++ Astronomy/Networking/MarsRoverClient.swift
@@ -5,31 +5,34 @@
 //  Created by Andrew R Madsen on 9/5/18.
 //  Copyright © 2018 Lambda School. All rights reserved.
 //
-
 import Foundation
 
 class MarsRoverClient {
-    
+
+    init(networkLoader: NetworkDataLoader = URLSession.shared) {
+        self.networkLoader = networkLoader
+    }
+
     func fetchMarsRover(named name: String,
                         using session: URLSession = URLSession.shared,
                         completion: @escaping (MarsRover?, Error?) -> Void) {
-        
+
         let url = self.url(forInfoForRover: name)
         fetch(from: url, using: session) { (dictionary: [String : MarsRover]?, error: Error?) in
 
-            guard let rover = dictionary?["photoManifest"] else {
+            guard let rover = dictionary?["photo_manifest"] else {
                 completion(nil, error)
                 return
             }
             completion(rover, nil)
         }
     }
-    
+
     func fetchPhotos(from rover: MarsRover,
                      onSol sol: Int,
                      using session: URLSession = URLSession.shared,
                      completion: @escaping ([MarsPhotoReference]?, Error?) -> Void) {
-        
+
         let url = self.url(forPhotosfromRover: rover.name, on: sol)
         fetch(from: url, using: session) { (dictionary: [String : [MarsPhotoReference]]?, error: Error?) in
             guard let photos = dictionary?["photos"] else {
@@ -39,23 +42,24 @@ class MarsRoverClient {
             completion(photos, nil)
         }
     }
-    
+
     // MARK: - Private
-    
+
     private func fetch<T: Codable>(from url: URL,
-                           using session: URLSession = URLSession.shared,
-                           completion: @escaping (T?, Error?) -> Void) {
-        session.dataTask(with: url) { (data, response, error) in
+                                   using session: URLSession = URLSession.shared,
+                                   completion: @escaping (T?, Error?) -> Void) {
+
+        networkLoader.loadData(from: url) { (data, error) in
             if let error = error {
                 completion(nil, error)
                 return
             }
-            
+
             guard let data = data else {
                 completion(nil, NSError(domain: "com.LambdaSchool.Astronomy.ErrorDomain", code: -1, userInfo: nil))
                 return
             }
-            
+
             do {
                 let jsonDecoder = MarsPhotoReference.jsonDecoder
                 let decodedObject = try jsonDecoder.decode(T.self, from: data)
@@ -63,11 +67,13 @@ class MarsRoverClient {
             } catch {
                 completion(nil, error)
             }
-        }.resume()
+
+        }
     }
-    
+
     private let baseURL = URL(string: "https://api.nasa.gov/mars-photos/api/v1")!
     private let apiKey = "qzGsj0zsKk6CA9JZP1UjAbpQHabBfaPg2M5dGMB7"
+    let networkLoader: NetworkDataLoader
 
     private func url(forInfoForRover roverName: String) -> URL {
         var url = baseURL
@@ -77,7 +83,7 @@ class MarsRoverClient {
         urlComponents.queryItems = [URLQueryItem(name: "api_key", value: apiKey)]
         return urlComponents.url!
     }
-    
+
     private func url(forPhotosfromRover roverName: String, on sol: Int) -> URL {
         var url = baseURL
         url.appendPathComponent("rovers")
---
+++ Astronomy/Networking/NetworkDataLoader.swift
@@ -0,0 +1,52 @@
+//
+//  NetworkDataLoader.swift
+//  Astronomy
+//
+//  Created by Jonathan Ferrer on 6/24/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+import Foundation
+
+protocol NetworkDataLoader {
+    func loadData(from request: URLRequest, completion: @escaping(Data?, Error?) -> Void)
+    func loadData(from url: URL, completion: @escaping(Data?, Error?) -> Void)
+}
+
+extension URLSession: NetworkDataLoader {
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+            guard let data = data else {
+                completion(nil, NSError(domain: "Corrupt Data", code: -1))
+                return
+            }
+            completion(data, error)
+        }.resume()
+
+    }
+
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+            guard let data = data else {
+                completion(nil, NSError(domain: "Corrupt Data", code: -1))
+                return
+            }
+            completion(data, error)
+        }.resume()
+    }
+
+
+}
