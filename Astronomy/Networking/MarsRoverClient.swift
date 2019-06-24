7d7b051ea185a390c714bb367ac4c7f24a8e0fca
Ryan Murphy <thomasryanmurphy@gmail.com>
2019-06-24 19:04:33 -0400

    I haven't gotten the error one working just yet, but I will.

--- Astronomy/Networking/MarsRoverClient.swift
+++ Astronomy/Networking/MarsRoverClient.swift
@@ -10,6 +10,13 @@ import Foundation
 
 class MarsRoverClient {
     
+    let networkLoader: NetworkDataLoader
+    
+    init(networkLoader: NetworkDataLoader = URLSession.shared) {
+        self.networkLoader = networkLoader
+    }
+    
+    
     func fetchMarsRover(named name: String,
                         using session: URLSession = URLSession.shared,
                         completion: @escaping (MarsRover?, Error?) -> Void) {
@@ -17,7 +24,7 @@ class MarsRoverClient {
         let url = self.url(forInfoForRover: name)
         fetch(from: url, using: session) { (dictionary: [String : MarsRover]?, error: Error?) in
 
-            guard let rover = dictionary?["photoManifest"] else {
+            guard let rover = dictionary?["photo_manifest"] else {
                 completion(nil, error)
                 return
             }
@@ -43,9 +50,11 @@ class MarsRoverClient {
     // MARK: - Private
     
     private func fetch<T: Codable>(from url: URL,
-                           using session: URLSession = URLSession.shared,
-                           completion: @escaping (T?, Error?) -> Void) {
-        session.dataTask(with: url) { (data, response, error) in
+                                   using session: URLSession = URLSession.shared,
+                                   completion: @escaping (T?, Error?) -> Void) {
+        
+        networkLoader.loadData(from: url) { (data, error) in
+            
             if let error = error {
                 completion(nil, error)
                 return
@@ -63,7 +72,7 @@ class MarsRoverClient {
             } catch {
                 completion(nil, error)
             }
-        }.resume()
+        }
     }
     
     private let baseURL = URL(string: "https://api.nasa.gov/mars-photos/api/v1")!
