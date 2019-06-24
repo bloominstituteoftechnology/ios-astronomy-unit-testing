4c9bb1214ca58ab49ee7992fd172d6fc71c181bf
jayfenam <jonathanferrer95@gmail.com>
2019-06-24 19:02:05 -0400

    fixed one thing

--- Astronomy/Networking/NetworkDataLoader.swift
+++ Astronomy/Networking/NetworkDataLoader.swift
@@ -8,9 +8,6 @@
 
 import Foundation
 
-
-import Foundation
-
 protocol NetworkDataLoader {
     func loadData(from request: URLRequest, completion: @escaping(Data?, Error?) -> Void)
     func loadData(from url: URL, completion: @escaping(Data?, Error?) -> Void)
