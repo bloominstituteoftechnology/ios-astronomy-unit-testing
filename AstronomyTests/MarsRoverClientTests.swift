73affbd2ea9a525245a4db5d53c77e995b7eb5c0
Mitchell Budge <mitchellbudge@gmail.com>
2019-06-24 13:39:06 -0600

    Refactor MarsRoverClient fetch<T> method

--- AstronomyTests/MarsRoverClientTests.swift
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -1,5 +1,5 @@
 //
-//  AstronomyTests.swift
+//  MarsRoverClientTests.swift
 //  AstronomyTests
 //
 //  Created by Mitchell Budge on 6/24/19.
@@ -9,7 +9,7 @@
 import XCTest
 @testable import Astronomy
 
-class AstronomyTests: XCTestCase {
+class MarsRoverClientTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
