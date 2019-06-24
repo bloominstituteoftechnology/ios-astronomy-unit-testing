ac541abb1557a5215949c337a645fd33a601f845
Hayden Hastings <haydenhastings32@gmail.com>
2019-06-24 15:23:39 -0600

    Deleted and added file in AstronomyTests file.

---
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -0,0 +1,16 @@
+//
+//  MarsRoverClientTests.swift
+//  AstronomyTests
+//
+//  Created by Hayden Hastings on 6/24/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class MarsRoverClientTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+}
