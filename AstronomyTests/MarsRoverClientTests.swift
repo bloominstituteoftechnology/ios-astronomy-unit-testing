817136a0168740d4af68374a5c1c43be831279cd
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-24 15:50:36 -0700

    MarsRoverClientTests

---
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -0,0 +1,22 @@
+//
+//  MarsRoverClientTests.swift
+//  AstronomyTests
+//
+//  Created by Hector Steven on 6/24/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class MarsRoverClientTests: XCTestCase {
+	
+	func testFetchMarsRover() {
+		
+	}
+	
+	func testFetchPhotos() {
+		
+	}
+	
+
+}
