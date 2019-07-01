980f44dc7b4be3e628e6ef87fe88341d6aa8dec9
Michael R.F <mrflowers22@uwalumni.com>
2019-07-01 14:37:17 -0700

    now I added the MRC test file

---
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -0,0 +1,21 @@
+//
+//  MarsRoverClientTests.swift
+//  AstronomyTests
+//
+//  Created by Michael Flowers on 7/1/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class MarsRoverClientTests: XCTestCase {
+
+    func testFetchMarsRover(){
+        
+    }
+   
+    func testFetchPhotos(){
+        
+    }
+
+}
