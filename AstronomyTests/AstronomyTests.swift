f31c7ae619e2a8ff565bf53dfa7d992b1039404f
MasterYourMoney <seschwan@yahoo.com>
2019-09-04 22:15:53 -0600

    MVP

--- AstronomyTests/AstronomyTests.swift
+++ AstronomyTests/AstronomyTests.swift
@@ -29,4 +29,31 @@ class AstronomyTests: XCTestCase {
         XCTAssertTrue(self.rover!.numberOfPhotos > 1)
     }
 
+    
+    func testFetchPhotos() {
+        let mock = MockDataLoader()
+        mock.data = validRoverJSON
+        
+        let controller = MarsRoverClient(networkLoader: mock)
+        let e = expectation(description: "Wait for results")
+        
+        controller.fetchMarsRover(named: "curiosity") { (data, error) in
+            e.fulfill()
+            self.rover = data
+        }
+        
+        wait(for: [e], timeout: 2)
+        
+        XCTAssertTrue(self.rover!.name == "Curiosity")
+        XCTAssertTrue(self.rover!.numberOfPhotos > 1, "Expecting total photos greater than 1")
+        mock.data = validSol1JSON
+        let photosController = MarsRoverClient(networkLoader: mock)
+        let photosExpection = expectation(description: "Wait for results")
+        photosController.fetchPhotos(from: self.rover!, onSol: 1) { (data, error) in
+            photosExpection.fulfill()
+            self.photos = data
+        }
+        wait(for: [photosExpection], timeout: 2)
+        XCTAssertTrue(self.photos!.count > 0, "Expecting multiple photos")
+    }
 }
