fad4e0225cfdeaff729b043c3264c9135dc7b891
bradleyyin <bradleyyin@gmail.com>
2019-09-16 15:48:18 -0700

    implement fetch photo tested implement fetch rover tested

--- AstronomyTests/MarsRoverClientTests.swift
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -12,10 +12,43 @@ import XCTest
 
 class MarsRoverClientTests: XCTestCase {
     func testFetchPhotos() {
+        var mockLoader = MockLoader()
+        mockLoader.data = validRoverJSON
+        let marsRoverClient = MarsRoverClient(loader: mockLoader)
+        let didFinishRover = expectation(description: "didFinishRover")
+        let didFinishPhoto = expectation(description: "didFinishPhoto")
+        marsRoverClient.fetchMarsRover(named: "curiosity") { (rover, error) in
+            
+            XCTAssertEqual("Curiosity", rover?.name)
+            XCTAssertEqual(10, rover?.maxSol)
+            guard let rover = rover else { XCTFail(); return}
+            
+            mockLoader.data = validSol1JSON
+            let marsRoverClient2 = MarsRoverClient(loader: mockLoader)
+            
+            marsRoverClient2.fetchPhotos(from: rover, onSol: 1) { (photos, error) in
+                XCTAssertEqual(4477, photos?[0].id)
+                didFinishPhoto.fulfill()
+            }
+            
+            didFinishRover.fulfill()
+        }
         
+        waitForExpectations(timeout: 5)
     }
     
     func testFetchMarsRover() {
-        let marsRoverClient = MarsRoverClient(loader: <#T##NetworkDataLoader#>)
+        var mockLoader = MockLoader()
+        mockLoader.data = validRoverJSON
+        let marsRoverClient = MarsRoverClient(loader: mockLoader)
+        let didFinish = expectation(description: "didFinish")
+        marsRoverClient.fetchMarsRover(named: "curiosity") { (rover, error) in
+            XCTAssertEqual("Curiosity", rover?.name)
+            XCTAssertEqual(10, rover?.maxSol)
+            didFinish.fulfill()
+        }
+        waitForExpectations(timeout: 5)
+        
     }
+    
 }
