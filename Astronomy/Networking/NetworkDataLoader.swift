2268ffce859618c71a184fef1bc02d44760bd907
Jeffrey <SantanaJny@gmail.com>
2019-09-16 17:18:48 -0400

    Refactoring MarsRoverClient
    adding networkDataLoader and applying it

--- Astronomy/Networking/NetworkDataLoader.swift
+++ Astronomy/Networking/NetworkDataLoader.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+protocol NetworkDataLoader {
+	func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void)
+	
+	func loadData(from request: URL, completion: @escaping (Data?, Error?) -> Void)
+}
+
+extension URLSession: NetworkDataLoader {
+	func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+		dataTask(with: request) { (data, _, error) in
+			completion(data, error)
+		}.resume()
+	}
+	
+	func loadData(from request: URL, completion: @escaping (Data?, Error?) -> Void) {
+		dataTask(with: request) { (data, _, error) in
+			completion(data, error)
+		}.resume()
+	}
+}
