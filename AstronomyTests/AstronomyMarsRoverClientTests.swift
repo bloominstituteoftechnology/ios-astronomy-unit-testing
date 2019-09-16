e397ca9b215274cbc5f4469bfd38dbd88044d867
Marlon <marmoneymar@icloud.com>
2019-09-16 16:27:48 -0400

    Added test target and NetworkDataLoader

---
+++ AstronomyTests/AstronomyMarsRoverClientTests.swift
@@ -0,0 +1,23 @@
+//
+//  AstronomyMarsRoverClientTests.swift
+//  AstronomyTests
+//
+//  Created by Marlon Raskin on 9/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import Astronomy
+
+class AstronomyMarsRoverClientTests: XCTestCase {
+
+	
+	func testMarsRoverClient() {
+		let marsRoverClient = MarsRoverClient()
+	}
+
+
+
+
+}
