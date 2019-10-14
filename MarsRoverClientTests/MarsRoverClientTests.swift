e0e8ba73cb85b873a6da345f895b701c5d25dacd
Jake <jlconnerly@gmail.com>
2019-10-14 17:13:56 -0500

    Testing fetching photos

--- MarsRoverClientTests/MarsRoverClientTests.swift
+++ MarsRoverClientTests/MarsRoverClientTests.swift
@@ -25,7 +25,41 @@ class MarsRoverClientTests: XCTestCase {
             }
         }
         wait(for: [resultsExpectation], timeout: 2)
+        
         XCTAssertNotNil(rover)
+        XCTAssertEqual(rover?.name, "Curiosity")
+        XCTAssertEqual(rover?.solDescriptions.count, 4)
+    }
+    
+    func testFetchingPhotos() {
+        let mock = MockDataLoader()
+        mock.data = validRoverJSON
+        let client = MarsRoverClient(networkDataLoader: mock)
+        var rover: MarsRover? = nil
+        var photoRefs: [MarsPhotoReference]? = nil
+        let roverResultExpectation = expectation(description: "Wait for rover")
+        let photoResultExpectation = expectation(description: "Wait for photo results")
+
+        client.fetchMarsRover(named: "Curiosity") { (marsRover, error) in
+            
+            DispatchQueue.main.async {
+                guard let rover1 = marsRover else { return }
+                rover = rover1
+                roverResultExpectation.fulfill()
+            }
+        }
+        
+        guard let rover2 = rover else { return }
+        
+        client.fetchPhotos(from: rover2, onSol: 0) { (marsPhotoReference, error) in
+            if let photos = marsPhotoReference {
+                photoResultExpectation.fulfill()
+                photoRefs = photos
+            }
+        }
+        wait(for: [roverResultExpectation, photoResultExpectation], timeout: 10)
+        
+        XCTAssertNotNil(photoRefs)
     }
     
     
