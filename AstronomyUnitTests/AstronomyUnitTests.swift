f2354ee13a7e09ff84d00c3ed9aeba4d5fcf11e8
Alexandra Rhodes <anrhodes5738@gmail.com>
2019-10-14 19:26:36 -0500

    fetchRover + fetchPhoto Unit Tests Complete

--- AstronomyUnitTests/AstronomyUnitTests.swift
+++ AstronomyUnitTests/AstronomyUnitTests.swift
@@ -7,15 +7,37 @@
 //
 
 import XCTest
+@testable import Astronomy
 
 class AstronomyUnitTests: XCTestCase {
-
+    
+    var rover: MarsRover?
+    
     func testFetchMarsRover() {
+        let mock = MockDataLoader()
+        mock.data = validRoverJSON
+        let controller = MarsRoverClient(dataLoader: mock)
+        let resultsExpectation = expectation(description: "wait for results")
+        
+        controller.fetchMarsRover(named: "Curiosity") { (rover, error) in
+            self.rover = rover
+            resultsExpectation.fulfill()
+        }
+        wait(for: [resultsExpectation], timeout: 2)
         
     }
     
     func testFetchPhotos() {
+        guard let rover = rover else {return}
+        let mock = MockDataLoader()
+        mock.data = validRoverJSON
+        let controller = MarsRoverClient(dataLoader: mock)
+        let resultsExpectation = expectation(description: "wait for results")
         
+        controller.fetchPhotos(from: rover, onSol: 1) { (photoReference, error) in
+            resultsExpectation.fulfill()
+        }
+        wait(for: [resultsExpectation], timeout: 2)
     }
     
 }
