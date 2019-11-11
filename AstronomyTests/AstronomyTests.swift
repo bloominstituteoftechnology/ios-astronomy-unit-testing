f1cb49d2327851098d71d9b586a4f02ce2b04f5f
Gi Pyo Kim <gipyok@gmail.com>
2019-11-11 13:00:01 -0800

    Part 1 finished

---
+++ AstronomyTests/AstronomyTests.swift
@@ -0,0 +1,20 @@
+//
+//  AstronomyTests.swift
+//  AstronomyTests
+//
+//  Created by Gi Pyo Kim on 11/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class AstronomyTests: XCTestCase {
+
+    /*
+     
+     Test cases:
+     
+     
+    */
+    
+}
