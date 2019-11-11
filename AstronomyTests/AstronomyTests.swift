bd071e385c67cbd61b72db19d04df8643c73cccf
Gi Pyo Kim <gipyok@gmail.com>
2019-11-11 13:30:08 -0800

    Passed the unit test for fetching mars rover from valid mock data

--- AstronomyTests/AstronomyTests.swift
+++ AstronomyTests/AstronomyTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import Astronomy
 
 class AstronomyTests: XCTestCase {
 
@@ -16,5 +17,34 @@ class AstronomyTests: XCTestCase {
      
      
     */
+    
+    var marsRover: MarsRover?
+    var error: Error?
+    
+    func testFetchMarsRover() {
+        let mock = MockDataLoader()
+        mock.data = validRoverJSON
+        
+        let controller = MarsRoverClient(networkLoader: mock)
+        let resultsExpection = expectation(description: "Wait for results")
+        
+        controller.fetchMarsRover(named: "Curiosity") { (marsRover, error) in
+            if let marsRover = marsRover {
+                self.marsRover = marsRover
+            }
+            
+            if let error = error {
+                self.error = error
+            }
+            
+            resultsExpection.fulfill()
+        }
+        
+        wait(for: [resultsExpection], timeout: 2)
+        
+        XCTAssertEqual("Curiosity", marsRover?.name)
+    }
+    
+
     
 }
