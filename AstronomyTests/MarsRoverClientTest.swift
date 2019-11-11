99b7c5439a789b5d40e9cf4d85d2a41ee1bf2dad
Wilma <kima_92@fastmail.com>
2019-11-11 18:02:03 -0500

    Added MarsRoverClientTests class.

---
+++ AstronomyTests/MarsRoverClientTest.swift
@@ -0,0 +1,32 @@
+//
+//  MarsRoverClientTest.swift
+//  AstronomyTests
+//
+//  Created by macbook on 11/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+@testable import Astronomy
+
+class MarsRoverClientTest: XCTestCase {
+    
+    //MARK: Properties
+    private let roverClkient = MarsRoverClient()
+    private let photoCollectionVC = PhotosCollectionViewController()
+    
+    
+    
+    func testFetchingPhotos() {
+        
+        
+        
+    }
+    
+    func testFetchingRover() {
+        
+        
+        
+    }
+    
+}
