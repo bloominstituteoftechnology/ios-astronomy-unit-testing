555f94d1a374a28bfbd5081214d87a0d2c06b01c
Jerry Haaser <jerry.haaser@hotmail.com>
2019-11-11 16:42:48 -0600

    added unit test target, added NetworkDataLoader and URLSession files

---
+++ AstronomyTests/AstronomyTests.swift
@@ -0,0 +1,19 @@
+//
+//  AstronomyTests.swift
+//  AstronomyTests
+//
+//  Created by Jerry haaser on 11/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class AstronomyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+
+
+}
