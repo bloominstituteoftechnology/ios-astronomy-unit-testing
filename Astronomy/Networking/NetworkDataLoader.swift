5288578de9961acb01c1c578a15df428344bf5d1
Alex Shillingford <alex.shillingford@icloud.com>
2019-11-11 13:52:07 -0700

    Create NetworkDataLoader.swift file with NetworkDataLoader protocol and two loadData() functions, one taking URLRequest, the other taking URL as a parameter

---
+++ Astronomy/Networking/NetworkDataLoader.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkDataLoader.swift
+//  Astronomy
+//
+//  Created by Alex Shillingford on 11/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkDataLoader {
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void)
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void)
+}
