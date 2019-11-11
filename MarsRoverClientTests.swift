074796c30abfc5dbb3f1a9014bbd76b321feced4
Isvvc <lyons@tuta.io>
2019-11-11 15:13:02 -0700

    Test fetching sol 1 photos

--- MarsRoverClientTests.swift
+++ MarsRoverClientTests.swift
@@ -27,5 +27,31 @@ class MarsRoverClientTests: XCTestCase {
         
         wait(for: [resultsExpectation], timeout: 2)
     }
+    
+    func testFetchPhotos() {
+        let mock = MockLoader()
+        mock.data = validRoverJSON
+        
+        let client = MarsRoverClient(networkLoader: mock)
+        
+        let resultsExpectation = expectation(description: "Wait for the results")
+        client.fetchMarsRover(named: "Curiosity") { (rover, error) in
+            guard let rover = rover else {
+                XCTFail("Rover fetch failed.")
+                return
+            }
+            
+            mock.data = validSol1JSON
+            let sol1Client = MarsRoverClient(networkLoader: mock)
+            sol1Client.fetchPhotos(from: rover, onSol: 1) { (photos, error) in
+                XCTAssertNotNil(photos)
+                XCTAssertEqual(photos?.count, 16)
+                resultsExpectation.fulfill()
+            }
+        }
+        
+        
+        wait(for: [resultsExpectation], timeout: 2)
+    }
 
 }
