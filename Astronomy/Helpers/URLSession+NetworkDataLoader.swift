aedb35e3cb43d9e514084c7e3a3ed2e3129acc38
Wilma <kima_92@fastmail.com>
2019-11-11 14:43:12 -0500

    Added extension on URLSession conforming to NetworkDataLoader.

---
+++ Astronomy/Helpers/URLSession+NetworkDataLoader.swift
@@ -0,0 +1,22 @@
+//
+//  URLSession+NetworkDataLoader.swift
+//  Astronomy
+//
+//  Created by macbook on 11/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+extension URLSession: NetworkDataLoader {
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        <#code#>
+    }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        <#code#>
+    }
+    
+    
+}
