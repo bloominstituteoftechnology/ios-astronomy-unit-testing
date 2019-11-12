8d2fa8484deb9002ad6893df6f304f0492d21873
Jerry Haaser <jerry.haaser@hotmail.com>
2019-11-11 23:53:35 -0600

    updated mockJSON

--- AstronomyTests/AstronomyTests.swift
+++ AstronomyTests/AstronomyTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import Astronomy
 
 class AstronomyTests: XCTestCase {
     
@@ -19,11 +20,68 @@ class AstronomyTests: XCTestCase {
     func testFetchMarsRover() {
         
         let mock = MockLoader()
-        mock.data 
+        mock.data = validRoverJSON
+        
+        let controller = MarsRoverClient(networkLoader: mock)
+        
+        let resultsExpectation = expectation(description: "Wait for results")
+        controller.fetchMarsRover(named: "Curiostiy") { (rover, error) in
+            if let rover = rover {
+                resultsExpectation.fulfill()
+                self.roverCheck = rover
+            }
+            
+            if let error = error {
+                print(error)
+            }
+        }
+        wait(for: [resultsExpectation], timeout: 2)
+        XCTAssertNotNil(roverCheck)
+        
     }
     
     func testFetchPhotos() {
         
+        let mock = MockLoader()
+        mock.data = validRoverJSON
+        
+        let controller = MarsRoverClient(networkLoader: mock)
+        var photo: [MarsPhotoReference]?
+        let resultsExpectation = expectation(description: "waitfor results")
+        
+        guard let rover = roverCheck else { return }
+        
+        controller.fetchPhotos(from: rover, onSol: 1) { (photoReference, error) in
+            if let photos = photoReference {
+                resultsExpectation.fulfill()
+                photo = photos
+            }
+            resultsExpectation.fulfill()
+        }
+        wait(for: [resultsExpectation], timeout: 2)
+        
+        XCTAssertNotNil(photo)
+    }
+    
+    func testBadFetch() {
+        let mock = MockLoader()
+               mock.data = 
+               
+               let controller = MarsRoverClient(networkLoader: mock)
+               
+               let resultsExpectation = expectation(description: "Wait for results")
+               controller.fetchMarsRover(named: "Curiostiy") { (rover, error) in
+                   if let rover = rover {
+                       resultsExpectation.fulfill()
+                       self.roverCheck = rover
+                   }
+                   
+                   if let error = error {
+                       print(error)
+                   }
+               }
+               wait(for: [resultsExpectation], timeout: 2)
+               XCTAssertNotNil(roverCheck)
     }
 
 }
