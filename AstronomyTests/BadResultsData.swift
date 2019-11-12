859811fa15e8c6f09f148fce341f49653a83b590
Wilma <kima_92@fastmail.com>
2019-11-12 09:46:08 -0500

    Added JSON to GoodResults and BadResults Data.

--- AstronomyTests/BadResultsData.swift
+++ AstronomyTests/BadResultsData.swift
@@ -9,5 +9,262 @@
 import Foundation
 
 let badResultsData = """
+{
+  "photo_manifest": {
+    "name": "Curiosity",
+    "landing_date": "2012-08-06",
+    "launch_date": "2011-11-26",
+    "status": "active",
+    "max_sol": 2540,
+    "max_date": "2019-09-28",
+    "total_photos": 366206,
+    "photos": [
+      {
+        "sol": 0,
+        "earth_date": "2012-08-06",
+        "total_photos": 3702,
+        "cameras": [
+          "CHEMCAM",
+          "FHAZ",
+          "MARDI",
+          "RHAZ"
+        ]
+      },
+      {
+        "sol": 1,
+        "earth_date": "2012-08-07",
+        "total_photos": 16,
+        "cameras": [
+          "MAHLI",
+          "MAST",
+          "NAVCAM"
+        ]
+      },
+      {
+        "sol": 2,
+        "earth_date": "2012-08-08",
+        "total_photos": 74,
+        "cameras": [
+          "NAVCAM"
+        ]
+      },
+      {
+        "sol": 3,
+        "earth_date": "2012-08-09",
+        "total_photos": 338,
+        "cameras": [
+          "MAST"
+        ]
+      },
+      {
+        "sol": 10,
+        "earth_date": "2012-08-16",
+        "total_photos": 26,
+        "cameras": [
+          "CHEMCAM",
+          "MAHLI",
+          "NAVCAM"
+        ]
+      },
+      {
+        "sol": 12,
+        "earth_date": "2012-08-18",
+        "total_photos": 32,
+        "cameras": [
+          "CHEMCAM",
+          "NAVCAM"
+        ]
+      },
+      {
+        "sol": 13,
+        "earth_date": "2012-08-19",
+        "total_photos": 208,
+        "cameras": [
+          "CHEMCAM",
+          "MAST",
+          "NAVCAM"
+        ]
+      },
+      {
+        "sol": 14,
+        "earth_date": "2012-08-20",
+        "total_photos": 70,
+        "cameras": [
+          "CHEMCAM",
+          "FHAZ",
+          "MAST",
+          "NAVCAM"
+        ]
+      },
+      {
+        "sol": 15,
+        "earth_date": "2012-08-21",
+        "total_photos": 58,
+        "cameras": [
+          "CHEMCAM",
+          "FHAZ",
+          "NAVCAM",
+          "RHAZ"
+        ]
+      },
+      {
+        "sol": 16,
+        "earth_date": "2012-08-22",
+        "total_photos": 78,
+        "cameras": [
+          "FHAZ",
+          "NAVCAM",
+          "RHAZ"
+        ]
+      },
+      {
+        "sol": 17,
+        "earth_date": "2012-08-23",
+        "total_photos": 273,
+        "cameras": [
+          "MAST",
+          "NAVCAM"
+        ]
+      },
+      {
+        "sol": 19,
+        "earth_date": "2012-08-25",
+        "total_photos": 444,
+        "cameras": [
+          "CHEMCAM",
+          "MAST"
+        ]
+      },
+      {
+        "sol": 20,
+        "earth_date": "2012-08-26",
+        "total_photos": 1046,
+        "cameras": [
+          "MAST"
+        ]
+      },
+      {
+        "sol": 21,
+        "earth_date": "2012-08-27",
+        "total_photos": 73,
+        "cameras": [
+          "FHAZ",
+          "MARDI",
+          "MAST",
+          "NAVCAM",
+          "RHAZ"
+        ]
+      },
+      {
+        "sol": 22,
+        "earth_date": "2012-08-28",
+        "total_photos": 52,
+        "cameras": [
+          "CHEMCAM",
+          "FHAZ",
+          "NAVCAM",
+          "RHAZ"
+        ]
+      },
+      {
+        "sol": 23,
+        "earth_date": "2012-08-29",
+        "total_photos": 379,
+        "cameras": [
+          "MARDI",
+          "MAST",
+          "NAVCAM"
+        ]
+      },
+      {
+        "sol": 24,
+        "earth_date": "2012-08-30",
+        "total_photos": 218,
+        "cameras": [
+          "FHAZ",
+          "MARDI",
+          "MAST",
+          "NAVCAM",
+          "RHAZ"
+        ]
+      },
+      {
+        "sol": 25,
+        "earth_date": "2012-08-31",
+        "total_photos": 382,
+        "cameras": [
+          "MARDI",
+          "MAST",
+          "NAVCAM"
+        ]
+      },
+      {
+        "sol": 26,
+        "earth_date": "2012-09-01",
+        "total_photos": 76,
+        "cameras": [
+          "FHAZ",
+          "NAVCAM",
+          "RHAZ"
+        ]
+      },
+      {
+        "sol": 27,
+        "earth_date": "2012-09-02",
+        "total_photos": 98,
+        "cameras": [
+          "CHEMCAM",
+          "MARDI",
+          "MAST",
+          "NAVCAM"
+        ]
+      },
+      {
+        "sol": 29,
+        "earth_date": "2012-09-04",
+        "total_photos": 68,
+        "cameras": [
+          "FHAZ",
+          "NAVCAM",
+          "RHAZ"
+        ]
+      },
+      {
+        "sol": 30,
+        "earth_date": "2012-09-05",
+        "total_photos": 87,
+        "cameras": [
+          "CHEMCAM",
+          "FHAZ",
+          "MARDI",
+          "MAST",
+          "NAVCAM"
+        ]
+      },
+      {
+        "sol": 32,
+        "earth_date": "2012-09-07",
+        "total_photos": 184,
+        "cameras": [
+          "CHEMCAM",
+          "MAHLI",
+          "MARDI",
+          "MAST",
+          "NAVCAM"
+        ]
+      },
+      {
+        "sol": 33,
+        "earth_date": "2012-09-08",
+        "total_photos": 94,
+        "cameras": [
+          "CHEMCAM",
+          "MAHLI",
+          "MAST",
+          "NAVCAM"
+        ]
+      }
+    ]
+  }
 
 """.data(using: .utf8)!
