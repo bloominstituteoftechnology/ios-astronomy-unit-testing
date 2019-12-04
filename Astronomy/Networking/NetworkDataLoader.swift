6a5a922d9cd99c41b8e58fd5433e0e87e70c9ef6
John Kouris <jdkouris@gmail.com>
2019-12-03 20:36:35 -0800

    Added the extension to URLSession and made it conform to NetworkDataLoader

--- Astronomy/Networking/NetworkDataLoader.swift
+++ Astronomy/Networking/NetworkDataLoader.swift
@@ -12,3 +12,19 @@ protocol NetworkDataLoader {
     func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void)
     func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void)
 }
+
+extension URLSession: NetworkDataLoader {
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        let dataTask = URLSession.shared.dataTask(with: request) { (data, _, error) in
+            completion(data, error)
+        }
+        dataTask.resume()
+    }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        let dataTask = URLSession.shared.dataTask(with: url) { (data, _, error) in
+            completion(data, error)
+        }
+        dataTask.resume()
+    }
+}
