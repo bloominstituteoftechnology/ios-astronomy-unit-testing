ec39908f8848b0068a766c78f0ea6f1a6535c8ba
dillonp23 <dillonp.lambda@gmail.com>
2019-12-04 20:56:06 -0800

    Implement unit test for fetching photos from sol for rover. All tests passing

--- AstronomyTests/AstronomyTests.swift
+++ AstronomyTests/AstronomyTests.swift
@@ -12,6 +12,7 @@ import XCTest
 
 class AstronomyTests: XCTestCase {
     
+    
     func testFetchMarsRover() {
         
         let controller = MarsRoverClient()
@@ -37,8 +38,47 @@ class AstronomyTests: XCTestCase {
     }
     
     func testFetchPhotos() {
+        var marsRover: MarsRover?
+        let controller = MarsRoverClient()
         
+        let roverExpectation = self.expectation(description: "Waiting for API to return rover results")
         
+        let bundle = Bundle(for: AstronomyTests.self)
+        let path = bundle.path(forResource: "roverJSON", ofType: "js")!
+        let fileData = try! Data(contentsOf: URL(fileURLWithPath: path))
+        
+        var mockLoader = MockLoader()
+        mockLoader.data = fileData
+        
+        controller.fetchMarsRover(named: "Curiosity") { (rover, error) in
+            
+            XCTAssertNotNil(rover)
+            marsRover = rover
+            XCTAssertNil(error)
+            roverExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 10, handler: nil)
+        
+        let solExpectation = self.expectation(description: "Waiting for API to return sol information")
+        
+        let solBundle = Bundle(for: AstronomyTests.self)
+        let solPath = solBundle.path(forResource: "solJSON", ofType: "js")!
+        let solFileData = try! Data(contentsOf: URL(fileURLWithPath: solPath))
+        
+        guard let curiosity = marsRover else { return }
+        
+        mockLoader.data = solFileData
+        
+        controller.fetchPhotos(from: curiosity, onSol: 1) { (photoReferences, error) in
+            
+            XCTAssertNotNil(photoReferences)
+            XCTAssertNil(error)
+            solExpectation.fulfill()
+            
+        }
+        
+        waitForExpectations(timeout: 10, handler: nil)
         
     }
     
