5cc926f4c4bf44501f13119471289aba3bf903cb
Alex Shillingford <alex.shillingford@icloud.com>
2019-12-16 16:12:57 -0700

    Create MarsRoverClientTests.swift target specifically for Mars Rover Client

---
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -0,0 +1,21 @@
+//
+//  MarsRoverClientTests.swift
+//  AstronomyTests
+//
+//  Created by Alex Shillingford on 12/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class MarsRoverClientTests: XCTestCase {
+
+    func testFetchMarsRover() {
+        
+    }
+    
+    func testFetchPhotos() {
+        
+    }
+
+}
