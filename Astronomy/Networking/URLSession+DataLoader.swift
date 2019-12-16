bec2abf6114540612a2aa062758e55d941b6e8cb
Jon Bash <jonbash@protonmail.com>
2019-12-16 12:28:35 -0800

    add URLSession conformance to DataLoader

--- Astronomy/Networking/URLSession+DataLoader.swift
+++ Astronomy/Networking/URLSession+DataLoader.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+
+extension URLSession: DataLoader {
+    func loadData(
+        with request: URLRequest,
+        completion: @escaping (Result<Data, Error>) -> Void)
+    {
+        dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode < 200 || response.statusCode > 299
+            {
+                completion(.failure(NetworkError.badResponse(response)))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NetworkError.noData(response)))
+                return
+            }
+            
+            completion(.success(data))
+        }.resume()
+    }
+    
+    func loadData(with url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+        let request = URLRequest(url: url)
+        loadData(with: request, completion: completion)
+    }
+}
