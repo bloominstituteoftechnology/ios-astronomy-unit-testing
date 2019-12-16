62f7ba91de9d59c363aba76c01b0d1a369ca9599
Dan M <djmthedev@gmail.com>
2019-12-16 14:37:28 -0800

    Parts 1 & 2

--- Astronomy/Networking/MarsRoverClient.swift
+++ Astronomy/Networking/MarsRoverClient.swift
@@ -10,64 +10,16 @@ import Foundation
 
 class MarsRoverClient {
     
-    func fetchMarsRover(named name: String,
-                        using session: URLSession = URLSession.shared,
-                        completion: @escaping (MarsRover?, Error?) -> Void) {
-        
-        let url = self.url(forInfoForRover: name)
-        fetch(from: url, using: session) { (dictionary: [String : MarsRover]?, error: Error?) in
-
-            guard let rover = dictionary?["photoManifest"] else {
-                completion(nil, error)
-                return
-            }
-            completion(rover, nil)
-        }
-    }
-    
-    func fetchPhotos(from rover: MarsRover,
-                     onSol sol: Int,
-                     using session: URLSession = URLSession.shared,
-                     completion: @escaping ([MarsPhotoReference]?, Error?) -> Void) {
-        
-        let url = self.url(forPhotosfromRover: rover.name, on: sol)
-        fetch(from: url, using: session) { (dictionary: [String : [MarsPhotoReference]]?, error: Error?) in
-            guard let photos = dictionary?["photos"] else {
-                completion(nil, error)
-                return
-            }
-            completion(photos, nil)
-        }
-    }
-    
-    // MARK: - Private
-    
-    private func fetch<T: Codable>(from url: URL,
-                           using session: URLSession = URLSession.shared,
-                           completion: @escaping (T?, Error?) -> Void) {
-        session.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                completion(nil, error)
-                return
-            }
-            
-            guard let data = data else {
-                completion(nil, NSError(domain: "com.LambdaSchool.Astronomy.ErrorDomain", code: -1, userInfo: nil))
-                return
-            }
-            
-            do {
-                let jsonDecoder = MarsPhotoReference.jsonDecoder
-                let decodedObject = try jsonDecoder.decode(T.self, from: data)
-                completion(decodedObject, nil)
-            } catch {
-                completion(nil, error)
-            }
-        }.resume()
-    }
+    // MARK: - Properties
     
     private let baseURL = URL(string: "https://api.nasa.gov/mars-photos/api/v1")!
     private let apiKey = "qzGsj0zsKk6CA9JZP1UjAbpQHabBfaPg2M5dGMB7"
+    
+    let networkLoader: NetworkDataLoader
+    
+    init(networkLoader: NetworkDataLoader = URLSession.shared) {
+        self.networkLoader = networkLoader
+    }
 
     private func url(forInfoForRover roverName: String) -> URL {
         var url = baseURL
@@ -87,5 +39,83 @@ class MarsRoverClient {
         urlComponents.queryItems = [URLQueryItem(name: "sol", value: String(sol)),
                                     URLQueryItem(name: "api_key", value: apiKey)]
         return urlComponents.url!
+    }
+    
+    // MARK: - Methods
+    
+    func fetchMarsRover(named name: String,
+                        using session: URLSession = URLSession.shared,
+                        completion: @escaping (MarsRover?, Error?) -> Void) {
+        
+        let url = self.url(forInfoForRover: name)
+        fetch(from: url, using: session) { (dictionary: [String : MarsRover]?, error: Error?) in
+
+            guard let rover = dictionary?["photo_manifest"] else {
+                completion(nil, error)
+                return
+            }
+            completion(rover, nil)
+        }
+    }
+    
+    func fetchPhotos(from rover: MarsRover,
+                     onSol sol: Int,
+                     using session: URLSession = URLSession.shared,
+                     completion: @escaping ([MarsPhotoReference]?, Error?) -> Void) {
+        
+        let url = self.url(forPhotosfromRover: rover.name, on: sol)
+        fetch(from: url, using: session) { (dictionary: [String : [MarsPhotoReference]]?, error: Error?) in
+            guard let photos = dictionary?["photos"] else {
+                completion(nil, error)
+                return
+            }
+            completion(photos, nil) 
+        }
+    }
+    
+    // MARK: - Private
+    
+    private func fetch<T: Codable>(from url: URL,
+                           using session: URLSession = URLSession.shared,
+                           completion: @escaping (T?, Error?) -> Void) {
+//        session.dataTask(with: url) { (data, response, error) in
+//            if let error = error {
+//                completion(nil, error)
+//                return
+//            }
+//
+//            guard let data = data else {
+//                completion(nil, NSError(domain: "com.LambdaSchool.Astronomy.ErrorDomain", code: -1, userInfo: nil))
+//                return
+//            }
+//
+//            do {
+//                let jsonDecoder = MarsPhotoReference.jsonDecoder
+//                let decodedObject = try jsonDecoder.decode(T.self, from: data)
+//                completion(decodedObject, nil)
+//            } catch {
+//                completion(nil, error)
+//            }
+//        }.resume()
+        networkLoader.loadData(from: url) { (data, error) in
+            
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(nil, NSError(domain: "com.LambdaSchool.Astronomy.ErrorDomain", code: -1, userInfo: nil))
+                return
+            }
+            
+            do {
+                let jsonDecoder = MarsPhotoReference.jsonDecoder
+                let decodedObject = try jsonDecoder.decode(T.self, from: data)
+                completion(decodedObject, nil)
+            } catch {
+                completion(nil, error)
+            }
+        }
     }
 }
