c90fcf73651722acedfa23b631969e205de24379
Jon Bash <jonbash@protonmail.com>
2019-12-16 16:14:47 -0800

    reorganize tests, finish fetch rover tests

--- AstronomyTests/MarsRoverClientTests.swift
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -12,25 +12,93 @@ import XCTest
 class MarsRoverClientTests: XCTestCase {
     // MARK: - Setup
     
-    var roverClient: MarsRoverClient!
-    var expectation: XCTestExpectation!
+    var rover: MarsRover!
+    
+    var roverExpectation: XCTestExpectation!
+    var photosExpectation: XCTestExpectation!
     
     override func setUp() {
-        expectation = XCTestExpectation(description: "Waiting for mock data (simulating API request)")
+        roverExpectation = XCTestExpectation(description: "Waiting for mock rover (simulating API request)")
+        photosExpectation = XCTestExpectation(description: "Waiting for mock photo data (simulating API request")
     }
     
-    func mockRoverClient(withData data: Data) {
-        roverClient = MarsRoverClient(dataLoader: MockLoader(data: data))
-    }
+    // MARK: - Fetch Rover Tests
     
-    // MARK: - Tests
-    
-    func testFetchRoverSuccess() {
-        mockRoverClient(withData: MockData.validRoverJSON)
-        
-        var rover: MarsRover? = nil
+    func testFetchRoverValid() {
+        let roverClient = mockClient(withData: MockData.validRoverJSON)
         
         roverClient.fetchMarsRover(named: "curiosity") { result in
+            XCTAssertNoThrow(try? result.get())
+            let rover = try! result.get()
+            XCTAssertNotNil(rover)
+            self.roverExpectation.fulfill()
+        }
+        wait(for: [roverExpectation], timeout: 3)
+        
+        let dataLoader = roverClient.dataLoader as! MockLoader
+        XCTAssertNil(dataLoader.error)
+        XCTAssertNotNil(dataLoader.data)
+    }
+    
+    func testFetchRoverInvalidJSON() {
+        let roverClient = mockClient(withData: MockData.invalidJSON)
+        
+        roverClient.fetchMarsRover(named: "curiosity") { result in
+            do {
+                XCTAssertThrowsError(try result.get())
+                let _ = try result.get()
+            } catch {
+                XCTAssertEqual(error as! NetworkError, NetworkError.noDecode)
+            }
+            self.roverExpectation.fulfill()
+        }
+        wait(for: [roverExpectation], timeout: 3)
+        
+        let dataLoader = roverClient.dataLoader as! MockLoader
+        // dataLoader will not catch the error; only when the MarsRoverClient tries to decode it will the error be thrown
+        XCTAssertNil(dataLoader.error)
+        XCTAssertNotNil(dataLoader.data)
+    }
+    
+    func testFetchRoverBadModel() {
+        let roverClient = mockClient(withData: MockData.validJSONBadModel)
+        
+        roverClient.fetchMarsRover(named: "curiosity") { result in
+            do {
+                XCTAssertThrowsError(try result.get())
+                let _ = try result.get()
+            } catch {
+                XCTAssertEqual(error as! NetworkError, NetworkError.noDecode)
+            }
+            self.roverExpectation.fulfill()
+        }
+        wait(for: [roverExpectation], timeout: 3)
+        
+        let dataLoader = roverClient.dataLoader as! MockLoader
+        // dataLoader will not catch the error; only when the MarsRoverClient tries to decode it will the error be thrown
+        XCTAssertNil(dataLoader.error)
+        XCTAssertNotNil(dataLoader.data)
+    }
+    
+    func testFetchRoverNoData() {
+        let roverClient = mockClient()
+        
+        roverClient.fetchMarsRover(named: "curiosity") { result in
+            do {
+                XCTAssertThrowsError(try result.get())
+                let _ = try result.get()
+            } catch {
+                XCTAssertEqual(error as! NetworkError, NetworkError.noData)
+            }
+            self.roverExpectation.fulfill()
+        }
+        wait(for: [roverExpectation], timeout: 3)
+        
+        let dataLoader = roverClient.dataLoader as! MockLoader
+        
+        XCTAssertNil(dataLoader.data)
+        XCTAssertEqual(dataLoader.error as! NetworkError, NetworkError.noData)
+    }
             XCTAssertNoThrow(try? result.get())
             rover = try! result.get()
             self.expectation.fulfill()
@@ -55,5 +123,23 @@ class MarsRoverClientTests: XCTestCase {
         wait(for: [expectation], timeout: 3)
     }
     
+    // MARK: - Helper Methods
     
+    func mockClient(
+        withData data: Data? = nil,
+        withError error: NetworkError? = nil
+    ) -> MarsRoverClient {
+        return MarsRoverClient(dataLoader: MockLoader(data: data, error: error))
+    }
+    
+    func getRover() {
+        mockClient(withData: MockData.validRoverJSON).fetchMarsRover(
+            named: "curiosity")
+        { result in
+            // no assertions necessary; there's another test for that
+            self.rover = try! result.get()
+            self.roverExpectation.fulfill()
+        }
+        wait(for: [roverExpectation], timeout: 3)
+    }
 }
