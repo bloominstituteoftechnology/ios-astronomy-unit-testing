97955b14b40f0ba152fa9a13979a4c8dc789c21d
Alex Shillingford <alex.shillingford@icloud.com>
2019-12-16 18:04:54 -0700

    Add two tests for fetchMarsRover(), one to test that it properly decodes, and one to test that bad data fails so it doesn't just pass anything

--- AstronomyTests/MarsRoverClientTests.swift
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -7,11 +7,44 @@
 //
 
 import XCTest
+@testable import Astronomy
 
 class MarsRoverClientTests: XCTestCase {
 
     func testFetchMarsRover() {
+        var mockLoader = MockLoader()
+        mockLoader.data = validRoverJSON
+        var marsRover: MarsRover?
         
+        let client = MarsRoverClient(networkLoader: mockLoader)
+        
+        let exp = expectation(description: "Test asynchronous code on fetchMarsRover()")
+        
+        client.fetchMarsRover(named: "Curiosity", using: mockLoader) { (rover, error) in
+            marsRover = rover
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 3)
+        XCTAssertNotNil(marsRover)
+    }
+    
+    func testFetchMarsRoverFail() {
+        var mockLoader = MockLoader()
+        mockLoader.data = invalidRoverJSON
+        var marsRover: MarsRover?
+        
+        let client = MarsRoverClient(networkLoader: mockLoader)
+        
+        let exp = expectation(description: "Test asynchronous code on fetchMarsRover()")
+        
+        client.fetchMarsRover(named: "Curiosity", using: mockLoader) { (rover, error) in
+            marsRover = rover
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 3)
+        XCTAssertNil(marsRover)
     }
     
     func testFetchPhotos() {
