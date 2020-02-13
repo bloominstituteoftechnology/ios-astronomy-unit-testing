eb2fe893877133d7075ff29711b248ae398dd83e
Alex Thompson <guitarman226@gmail.com>
2020-02-12 23:36:48 -0600

    Refactored code, initialized MarsRoverCLient to take a networkLoader object, gave the initializer a value or URLSession.shared, had errors, finally made it work.

---
+++ Astronomy/Utilities/URLSession+NetworkDataLoader.swift
@@ -0,0 +1,9 @@
+//
+//  URLSession+NetworkDataLoader.swift
+//  Astronomy
+//
+//  Created by Alex Thompson on 2/12/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
