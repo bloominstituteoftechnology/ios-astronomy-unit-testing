0884453620be5176be7e2ceabe0f950c3ffbb57a
Michael <maybemichael@icloud.com>
2020-02-24 22:52:23 -0800

    MVP

---
+++ Astronomy/Helpers/URLSession+NetworkDataLoader.swift
@@ -0,0 +1,23 @@
+//
+//  URLSession+NetworkDataLoader.swift
+//  Astronomy
+//
+//  Created by Michael on 2/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+extension URLSession: NetworkDataLoader {
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        dataTask(with: request) { data, _, error in
+            completion(data, error)
+        }.resume()
+    }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        dataTask(with: url) { data, _, error in
+            completion(data, error)
+        }.resume()
+    }
+}
---
+++ AstronomyTests/AstronomyTests.swift
@@ -0,0 +1,77 @@
+//
+//  AstronomyTests.swift
+//  AstronomyTests
+//
+//  Created by Michael on 2/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+@testable import Astronomy
+
+class AstronomyTests: XCTestCase {
+    
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.timeZone = TimeZone(abbreviation: "GMT")
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
+    
+    func testFetchMarsRover() {
+        var mock = MockDataLoader()
+        mock.data = validRoverJSON
+        let mrc = MarsRoverClient(networkLoader: mock)
+        var rover: MarsRover?
+        
+        let expectation = self.expectation(description: "Waiting for API to return Mars Rover data")
+        
+        mrc.fetchMarsRover(named: "Curiosity") { mRover, error in
+            expectation.fulfill()
+            rover = mRover
+        }
+        wait(for: [expectation], timeout: 3)
+        XCTAssertEqual(rover!.name, "Curiosity")
+        XCTAssertNotNil(rover)
+    }
+
+    func testFetchPhotos() {
+        var mock = MockDataLoader()
+        mock.data = validSol1JSON
+        let mrc = MarsRoverClient(networkLoader: mock)
+        
+        let rover = MarsRover(name: "Curiosity", launchDate: Date(), landingDate: Date(), status: .complete, maxSol: 7, maxDate: Date(), numberOfPhotos: 7, solDescriptions: [SolDescription(sol: 7, totalPhotos: 7, cameras: ["Middle Side Cam Located in the Back"])])
+        
+        var photoReferences: [MarsPhotoReference] = []
+        
+        let expectation = self.expectation(description: "Waiting for photo fetched photos")
+        
+        mrc.fetchPhotos(from: rover, onSol: 1) { (pics, error) in
+            expectation.fulfill()
+            photoReferences = pics!
+        }
+        wait(for: [expectation], timeout: 3)
+        XCTAssert(photoReferences.count > 0)
+    }
+    
+    func testDecodingRoverModel() {
+        var mock = MockDataLoader()
+        mock.data = validRoverJSON
+        let mrc = MarsRoverClient(networkLoader: mock)
+        var rover: MarsRover?
+        
+        mrc.fetchMarsRover(named: "Curiosity") { marsRover, error in
+            let url = Bundle.main.url(forResource: "MockJSON", withExtension: "json")
+            let data = try! Data(contentsOf: url!)
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            decoder.dateDecodingStrategy = .formatted(self.dateFormatter)
+            let marsRover = try! decoder.decode(MarsRover.self, from: data)
+                rover = marsRover
+            XCTAssertNotNil(rover)
+            XCTAssertEqual(rover!.name, "Curiosity")
+            XCTAssertEqual(rover?.maxSol, 10)
+        }
+    }
+}
