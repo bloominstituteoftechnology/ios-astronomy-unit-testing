0a5f9862a75edc45da514ae2e5638ac911baaab3
elizabeth paige <wingateep@Icloud.com>
2020-03-23 16:34:30 -0400

    set up networkDataLoader

---
+++ Astronomy/Resources/NetworkDataLoader.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkDataLoader.swift
+//  Astronomy
+//
+//  Created by Elizabeth Wingate on 3/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkDataLoader {
+    
+    func loadData(from request: URLRequest, completion: @escaping(Data?, Error?) -> Void)
+
+    func loadData(from url: URL, completion: @escaping(Data?, Error?) -> Void)
+    
+}
