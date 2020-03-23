c7fa187a2ff398acea62db97a5e4b90a435b34ad
Chris <chris.gonzales.82@gmail.com>
2020-03-23 15:01:04 -0700

    testForPhotoResults, testRoverRequestSpeed, and testPhotoRequestSpeed implemented.

--- MarsRoverClientTests/MarsRoverClientTests.swift
+++ MarsRoverClientTests/MarsRoverClientTests.swift
@@ -12,20 +12,57 @@ import XCTest
 class MarsRoverClientTests: XCTestCase {
     
     func testForRoverResults() {
+//        let mockDataLoader = MockDataLoader(data: validSol1JSON, error: nil)
+//        let controller = MarsRoverClient(dataLoader: mockDataLoader)
+        
         let controller = MarsRoverClient()
         
         let expectation = self.expectation(description: "Wait for results")
         
-        controller.fetchMarsRover(named: "Curiosity") {data,error in
-            XCTAssertNotNil(data)
-            XCTAssertNil(error)
-            expectation.fulfill()
+        controller.fetchMarsRover(named: "Curiosity") { (rover, error) in
+        XCTAssertNotNil(rover)
+        XCTAssertNil(error)
+        expectation.fulfill()
         }
+        
         wait(for: [expectation], timeout: 5)
     }
     
-    func testSpeedRequest() {
-        
+    func testForPhotoResults() {
+        let mockDataLoader = MockDataLoader(data: validSol1JSON, error: nil)
+        let controller = MarsRoverClient(dataLoader: mockDataLoader)
+
+        do{
+            let jsonDecoder = MarsPhotoReference.jsonDecoder
+            let rover = (try jsonDecoder.decode([String : MarsRover].self, from: validRoverJSON))["photo_manifest"]!
+            let expectation = self.expectation(description: "Wait for results")
+            controller.fetchPhotos(from:rover, onSol: 1) { (data, error) in
+                XCTAssertNotNil(data)
+                XCTAssertNil(error)
+                expectation.fulfill()
+            }
+            wait(for: [expectation], timeout: 5)
+        } catch {
+            XCTFail()
+        }
+    }
+    
+    func testRoverRequestSpeed() {
+        measureMetrics([.wallClockTime],
+                       automaticallyStartMeasuring: false) {
+                        self.startMeasuring()
+                        testForRoverResults()
+                        self.stopMeasuring()
+        }
+    }
+    
+    func testPhotoRequestSpeed(){
+        measureMetrics([.wallClockTime],
+                       automaticallyStartMeasuring: false) {
+                        self.startMeasuring()
+                        testForPhotoResults()
+                        self.stopMeasuring()
+        }
     }
     
     func testValidData() {
