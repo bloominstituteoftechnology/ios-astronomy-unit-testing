7844e23c71f98ddd42f8c04c073473c0910a1f12
Ufuk Türközü <ufuk_trkz@hotmail.de>
2020-03-23 20:55:56 +0100

    Added unit test target

---
+++ AstronomyTests/AstronomyTests.swift
@@ -0,0 +1,25 @@
+//
+//  AstronomyTests.swift
+//  AstronomyTests
+//
+//  Created by Ufuk Türközü on 23.03.20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+@testable import Astronomy
+
+class AstronomyTests: XCTestCase {
+    
+    /*
+     Does decoding work?
+     Does decoding fail when giving bad data?
+     Does it build the correct URL?
+     Does it build the correct URLRequest?
+     Are the search results saved properly?
+     Is the completion handler called when data is good?
+     Is the completion handler called when data is bad?
+     Is the completion handler called when networking fails?
+    */
+    
+}
