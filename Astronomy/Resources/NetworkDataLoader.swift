a678116a0f2344b7212266b5ac0a128ed14a333b
elizabeth paige <wingateep@Icloud.com>
2020-03-23 16:45:53 -0400

    made ext URLSession ++

--- Astronomy/Resources/NetworkDataLoader.swift
+++ Astronomy/Resources/NetworkDataLoader.swift
@@ -13,5 +13,20 @@ protocol NetworkDataLoader {
     func loadData(from request: URLRequest, completion: @escaping(Data?, Error?) -> Void)
 
     func loadData(from url: URL, completion: @escaping(Data?, Error?) -> Void)
-    
+}
+
+extension URLSession: NetworkDataLoader {
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        let task = self.dataTask(with: request) { (data, _, error) in
+            completion(data, error)
+        }
+        task.resume()
+    }
+
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        let task = self.dataTask(with: url) { (data, _, error) in
+            completion(data, error)
+        }
+        task.resume()
+    }
 }
