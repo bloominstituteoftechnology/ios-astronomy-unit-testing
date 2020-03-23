f266bcd27fa8d4de84f88188cb7a14a444806aef
Enrique Gongora <EGTech24@gmail.com>
2020-03-23 16:27:23 -0700

    completed MVP

--- AstronomyTests/MockDataLoader.swift
+++ AstronomyTests/MockDataLoader.swift
@@ -10,17 +10,19 @@ import Foundation
 @testable import Astronomy
 
 struct MockDataLoader: NetworkDataLoader {
-    let data: Data?
-    let error: Error?
-    
+
+    var data: Data?
+
+    var error: Error?
+
     func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 1) {
             completion(self.data, self.error)
         }
     }
-    
+
     func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 1) {
             completion(self.data, self.error)
         }
     }
