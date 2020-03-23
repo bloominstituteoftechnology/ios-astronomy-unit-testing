8b9d6cd2783a38e5996f6d7b56a47dfafb5270ea
Chris <chris.gonzales.82@gmail.com>
2020-03-23 12:43:55 -0700

    Created MarsRoverClientTest file, and stubbed out test methods

---
+++ MarsRoverClientTests/MarsRoverClientTests.swift
@@ -0,0 +1,33 @@
+//
+//  MarsRoverClientTests.swift
+//  MarsRoverClientTests
+//
+//  Created by Chris Gonzales on 3/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class MarsRoverClientTests: XCTestCase {
+
+    func testForSomeResults() {
+        
+    }
+    
+    func testSpeedRequest() {
+        
+    }
+    
+    func testValidData() {
+        
+    }
+    
+    func testInvalidJson() {
+        
+    }
+    
+    func testNoResultsData() {
+        
+    }
+
+}
