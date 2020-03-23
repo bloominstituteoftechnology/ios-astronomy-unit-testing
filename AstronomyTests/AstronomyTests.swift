8db041974cde43039c5bb316106f478439d071ac
Nick <ptnguyen1901@gmail.com>
2020-03-23 15:36:30 -0400

    Add MockLoader in UnitTest

--- AstronomyTests/AstronomyTests.swift
+++ AstronomyTests/AstronomyTests.swift
@@ -7,27 +7,18 @@
 //
 
 import XCTest
+@testable import Astronomy
 
-class AstronomyTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class MarsRoverClientTests : XCTestCase {
+
+    func testFetchMarRover() {
+        
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    
+    func testFetchPhotos() {
+        
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
