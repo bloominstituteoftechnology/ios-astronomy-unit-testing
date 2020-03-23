bbb3cc21f18a711e62e16635d4dd87757aba6a23
scottharris86 <scottharrisdev@gmail.com>
2020-03-23 18:55:25 -0400

    Part 1 - Prepare To Write Tests

---
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -0,0 +1,16 @@
+//
+//  AstronomyTests.swift
+//  AstronomyTests
+//
+//  Created by scott harris on 3/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+@testable import Astronomy
+
+class MarsRoverClientTests: XCTestCase {
+
+    
+
+}
