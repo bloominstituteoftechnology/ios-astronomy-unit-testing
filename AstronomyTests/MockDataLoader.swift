1598b5ac29c94252cc008a87effac0c56a4efe8f
Enrique Gongora <EGTech24@gmail.com>
2020-03-23 16:17:48 -0700

    MockDataLoader and test JSON added

---
+++ AstronomyTests/MockDataLoader.swift
@@ -0,0 +1,27 @@
+//
+//  MockDataLoader.swift
+//  AstronomyTests
+//
+//  Created by Enrique Gongora on 3/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+@testable import Astronomy
+
+struct MockDataLoader: NetworkDataLoader {
+    let data: Data?
+    let error: Error?
+    
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            completion(self.data, self.error)
+        }
+    }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            completion(self.data, self.error)
+        }
+    }
+}
