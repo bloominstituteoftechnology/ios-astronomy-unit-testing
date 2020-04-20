ac4475ff323cb0ae97cee06eaea6936524849bb2
Hector L <hectoremil21@hotmail.com>
2020-04-20 18:38:28 -0400

    Forgot to add .resume() to URLSession extension loadData(s)

--- Astronomy/Networking/NetworkDataLoader.swift
+++ Astronomy/Networking/NetworkDataLoader.swift
@@ -17,13 +17,13 @@ extension URLSession: NetworkDataLoader {
     func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
         dataTask(with: request) { (data, _, error) in
             completion(data, error)
-        }
+        }.resume()
     }
     
     func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
         dataTask(with: url) { (data, _, error) in
             completion(data, error)
-        }
+        }.resume()
     }
     
     
