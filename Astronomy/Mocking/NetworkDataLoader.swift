0f91fae7e203e243a25054a78008f984e16f7b55
Tobi Tselios <tobi.tselios@gmail.com>
2020-04-20 21:19:48 +0100

    Added a Unit Test Target and Created a NetworkDataLoader Protocol

---
+++ Astronomy/Mocking/NetworkDataLoader.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkDataLoader.swift
+//  Astronomy
+//
+//  Created by Tobi Kuyoro on 20/04/2020.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkDataLoader {
+    func loadData(from request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void)
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void)
+}
