e1f271cc769e3ce26688c95a700f47d226269a26
Hector L <hectoremil21@hotmail.com>
2020-04-20 19:53:10 -0400

    MockLoader implementation

---
+++ AstronomyTests/MockLoader.swift
@@ -0,0 +1,30 @@
+//
+//  MockLoader.swift
+//  AstronomyTests
+//
+//  Created by Karen Rodriguez on 4/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+@testable import Astronomy
+
+struct MockLoader: NetworkDataLoader {
+    
+    var data: Data?
+    var error: Error?
+    
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.005) {
+            completion(self.data, self.error)
+        }
+    }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+        completion(self.data, self.error)
+    }
+    }
+    
+    
+}
