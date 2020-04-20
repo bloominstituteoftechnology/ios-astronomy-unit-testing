66e43a25e668d004d4cb9e17da74bdc312606905
Tobi Tselios <tobi.tselios@gmail.com>
2020-04-20 23:11:45 +0100

    Created MockLoader

---
+++ AstronomyTests/MockLoader.swift
@@ -0,0 +1,33 @@
+//
+//  MockLoader.swift
+//  AstronomyTests
+//
+//  Created by Tobi Kuyoro on 20/04/2020.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+@testable import Astronomy
+
+class MockLoader: NetworkDataLoader {
+
+    let data: Data?
+    let error: Error?
+    
+    internal init(data: Data?, error: Error?) {
+        self.data = data
+        self.error = error
+    }
+    
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+            completion(self.data, self.error)
+        }
+    }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+            completion(self.data, self.error)
+        }
+    }
+}
