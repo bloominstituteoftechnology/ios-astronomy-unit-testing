4248ff763a35a8da0b7fea0a7cfc0b6d7d503d14
Shawn Gee <shawngee@gmail.com>
2020-04-20 17:29:21 -0400

    Added mock network session

---
+++ AstronomyTests/MockNetworkSession.swift
@@ -0,0 +1,60 @@
+//
+//  MockNetworkSession.swift
+//  AstronomyTests
+//
+//  Created by Shawn Gee on 4/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+@testable import Astronomy
+
+class MockNetworkSession: NetworkSession {
+    var dataTask: MockNetworkSessionDataTask!
+    
+    init(dataTask: MockNetworkSessionDataTask) {
+        self.dataTask = dataTask
+    }
+    
+    func networkDataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> NetworkSessionDataTask {
+        dataTask.completionHandler = completionHandler
+        return dataTask
+    }
+    
+    func networkDataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> NetworkSessionDataTask {
+        dataTask.completionHandler = completionHandler
+        return dataTask
+    }
+}
+
+class MockNetworkSessionDataTask: NetworkSessionDataTask {
+    func resume() {
+        timer = Timer.scheduledTimer(withTimeInterval: delay, repeats: false, block: { _ in
+            DispatchQueue.global().async {
+                self.completionHandler?(self.data, self.response, self.error)
+            }
+        })
+    }
+    
+    func cancel() {
+        timer?.invalidate()
+    }
+    
+    private var timer: Timer?
+    
+    let data: Data?
+    let response: URLResponse?
+    let error: Error?
+    
+    let delay: TimeInterval
+    
+    var completionHandler: ((Data?, URLResponse?, Error?) -> Void)?
+    
+    init(data: Data?, response: URLResponse?, error: Error?, delay: TimeInterval) {
+        self.data = data
+        self.response = response
+        self.error = error
+        
+        self.delay = delay
+    }
+}
