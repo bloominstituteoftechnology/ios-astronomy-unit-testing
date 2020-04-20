879abee5c978a27b32fa78b02fbd265318a99622
Wyatt Harrell <wyattaharrell@gmail.com>
2020-04-20 15:38:10 -0400

    Added new unit test target and associated files

---
+++ AstronomyTests/AstronomyTests.swift
@@ -0,0 +1,14 @@
+//
+//  AstronomyTests.swift
+//  AstronomyTests
+//
+//  Created by Wyatt Harrell on 4/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class AstronomyTests: XCTestCase {
+
+
+}
