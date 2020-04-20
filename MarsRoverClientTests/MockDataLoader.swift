28794aef1b212349574e7aa0384ce354a7cd3f13
Brad <braddiroff@icloud.com>
2020-04-20 16:29:47 -0400

    Creating mock data loader

---
+++ MarsRoverClientTests/MockDataLoader.swift
@@ -0,0 +1,30 @@
+//
+//  MockDataLoader.swift
+//  MarsRoverClientTests
+//
+//  Created by Bradley Diroff on 4/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+@testable import Astronomy
+
+class MockDataLoader: NetworkDataLoader {
+    
+    let data: Data?
+    let response: URLResponse?
+    let error: Error?
+    
+    internal init(data: Data?, response: URLResponse?, error: Error?) {
+        self.data = data
+        self.response = response
+        self.error = error
+    }
+    
+    func loadData(using request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.005) {
+            completion(self.data, self.response, self.error)
+        }
+    }
+}
