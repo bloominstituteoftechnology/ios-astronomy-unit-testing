410a9988b40c1ac86a27369af8638f6ef31b4f4b
Wyatt Harrell <wyattaharrell@gmail.com>
2020-04-20 16:40:47 -0400

    Added testFetchPhotos. MVP

--- AstronomyTests/AstronomyTests.swift
+++ AstronomyTests/AstronomyTests.swift
@@ -10,8 +10,7 @@ import XCTest
 @testable import Astronomy
 
 class AstronomyTests: XCTestCase {
-
-
+    
     func testFetchMarsRover() {
         let expectation = self.expectation(description: "Wait for results")
         let mockLoader = MockLoader(data: validRoverJSON, error: nil)
@@ -21,6 +20,7 @@ class AstronomyTests: XCTestCase {
             
             XCTAssertNil(error)
             XCTAssertNotNil(rover)
+            XCTAssertTrue(rover?.name == "Curiosity")
             
             expectation.fulfill()
         }
@@ -29,15 +29,43 @@ class AstronomyTests: XCTestCase {
     }
     
     func testFetchPhotos() {
-        let expectation = self.expectation(description: "Wait for results")
+        let roverExpectation = self.expectation(description: "Wait for rover")
         let mockLoader = MockLoader(data: validSol1JSON, error: nil)
         let controller = MarsRoverClient(networkLoader: mockLoader)
         
+        /*
+         FETCH A MARS ROVER
+        */
+        var marsRover: MarsRover?
         
+        let mockRoverLoader = MockLoader(data: validRoverJSON, error: nil)
+        let controller2 = MarsRoverClient(networkLoader: mockRoverLoader)
+        controller2.fetchMarsRover(named: "Curiosity") { (rover, error) in
+            
+            if let rover = rover {
+                marsRover = rover
+            }
+            
+            roverExpectation.fulfill()
+        }
         
+        wait(for: [roverExpectation], timeout: 10)
         
+        /*
+         FETCH PHOTOS
+        */
+        let photoExpectation = self.expectation(description: "Wait for photos")
+
+        controller.fetchPhotos(from: marsRover!, onSol: 1) { (marsPhotoReference, error) in
+           
+            XCTAssertNil(error)
+            XCTAssertNotNil(marsPhotoReference)
+            XCTAssertTrue(marsPhotoReference!.count > 0)
+            
+            photoExpectation.fulfill()
+        }
         
-        
+        wait(for: [photoExpectation], timeout: 10)
     }
     
     
