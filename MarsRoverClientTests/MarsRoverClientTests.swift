2fc49659716b7b7f7755a56b5b4c7d47faadc78c
Brad <braddiroff@icloud.com>
2020-04-20 17:41:26 -0400

    Completed project

--- MarsRoverClientTests/MarsRoverClientTests.swift
+++ MarsRoverClientTests/MarsRoverClientTests.swift
@@ -7,21 +7,79 @@
 //
 
 import XCTest
+@testable import Astronomy
 
 class MarsRoverClientTests: XCTestCase {
+    
+    // The rover part
+    
+    var marsRover: MarsRover?
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testCuriosityExample() {
+        let mockDataLoader = MockDataLoader(data: validRoverJSON, response: nil, error: nil)
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+        let expectation = self.expectation(description: "Need the rover")
+        let client = MarsRoverClient(dataLoader: mockDataLoader)
+        
+        client.fetchMarsRover(named: "curiosity") { (rover, error) in
+            
+            self.marsRover = rover
+            
+            XCTAssertEqual((self.marsRover?.name ?? "NO NAME"), "Curiosity", "Expected name Curiosity")
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 35)
+        
+    // The picture portion
+        
+        guard let rover = marsRover else {
+            XCTAssertEqual((self.marsRover?.name ?? "NO NAME"), "Curiosity", "🥳THERES NO ROVER DATA!!!")
+            return
+        }
+        
+         let picMockDataLoader = MockDataLoader(data: validSol1JSON, response: nil, error: nil)
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+         let picExpectation = self.expectation(description: "Need the sols")
+         let picClient = MarsRoverClient(dataLoader: picMockDataLoader)
+         
+        picClient.fetchPhotos(from: rover, onSol: 1) { (photoRefs, error) in
+            
+            XCTAssertGreaterThan(photoRefs?.count ?? 0, 0, "Expected to see some photo references")
+            
+            picExpectation.fulfill()
+            
+        }
+         
+         wait(for: [picExpectation], timeout: 35)
+        
     }
+    
+    // Does not work below
+    
+    func testSolData() {
+        
+        guard let rover = marsRover else {
+            XCTAssertEqual((self.marsRover?.name ?? "NO NAME"), "Curiosity", "🥳THERES NO ROVER DATA!!!")
+            return
+        }
+        
+         let mockDataLoader = MockDataLoader(data: validSol1JSON, response: nil, error: nil)
+
+         let expectation = self.expectation(description: "Need the sols")
+         let client = MarsRoverClient(dataLoader: mockDataLoader)
+         
+        client.fetchPhotos(from: rover, onSol: 1) { (photoRefs, error) in
+            
+            XCTAssertGreaterThan(photoRefs?.count ?? 0, 0, "Expected to see some photo references")
+            
+            expectation.fulfill()
+            
+        }
+         
+         wait(for: [expectation], timeout: 35)
+     }
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
