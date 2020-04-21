dd0d58d4b396e6d68dc20838d61b6cb2f8bea6da
Mark Gerrior <github@gerrior.com>
2020-04-20 22:27:58 -0700

    Testing fetchMarsRover() and fetchPhotos()

---
+++ MarsRoverClientTests/MarsRoverClientTests.swift
@@ -0,0 +1,64 @@
+//
+//  MarsRoverClientTests.swift
+//  MarsRoverClientTests
+//
+//  Created by Mark Gerrior on 4/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+// I don't think I appreciated this was the module name and not the class name.
+@testable import Astronomy
+
+class MarsRoverClientTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    func testFetchMarsRoverProduction() {
+        measure {
+            let expectation = self.expectation(description: "Wait for results")
+            let client = MarsRoverClient()
+
+            client.fetchMarsRover(named: "curiosity") { (rover, error) in
+                expectation.fulfill()
+            }
+            
+            wait(for: [expectation], timeout: 5)
+        }
+    }
+    
+    func testFetchPhotosProduction() {
+        measure {
+            var expectation = self.expectation(description: "Wait for fetchMarsRover")
+            let client = MarsRoverClient()
+            var roverInfo: MarsRover?
+            
+            // First load the rover for the next step.
+            client.fetchMarsRover(named: "curiosity") { (rover, error) in
+                roverInfo = rover
+                expectation.fulfill()
+            }
+            
+            wait(for: [expectation], timeout: 5)
+            
+            XCTAssertNotNil(roverInfo, "⚠️ Rover was unable to load from production")
+            
+            guard let rover = roverInfo else { return }
+            expectation = self.expectation(description: "Wait for fetchPhotos")
+
+            var photoReferences = [MarsPhotoReference]()
+            
+            // Next step, load the photos.
+            client.fetchPhotos(from: rover, onSol: rover.solDescriptions[7].sol) { (photoRefs, error) in
+                photoReferences = photoRefs ?? []
+                expectation.fulfill()
+            }
+            
+            wait(for: [expectation], timeout: 5)
+
+            XCTAssertTrue(photoReferences.count > 0, "⚠️ No photos returned from production")
+        }
+    }
+}
