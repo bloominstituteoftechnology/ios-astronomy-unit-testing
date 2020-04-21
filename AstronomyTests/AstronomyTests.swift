81693c95141f875ae0bfe8d5248183a5b839225c
Ezra Black <dev.ezrablack@gmail.com>
2020-04-20 21:52:19 -0500

    Added Testing for client.

--- AstronomyTests/AstronomyTests.swift
+++ AstronomyTests/AstronomyTests.swift
@@ -10,14 +10,14 @@ import XCTest
 @testable import Astronomy
 /*
 
-Does decoding work? 🛑
-Does decoding fail when given bad data?🛑
-Does it build the correct URL?🛑
-Does it build the correct URLRequest?🛑
-are the search results saved properly?🛑
-Is the completion handler called when data is good?🛑
-Is the completion handler called when data is bad?🛑
-Is the completion handler called when the network fails?🛑
+Does decoding work? ✅
+Does decoding fail when given bad data?✅
+Does it build the correct URL?✅
+Does it build the correct URLRequest?✅
+are the search results saved properly?✅
+Is the completion handler called when data is good?✅
+Is the completion handler called when data is bad?✅
+Is the completion handler called when the network fails?✅
 create expectation
 create controller
 schedule work
@@ -26,10 +26,84 @@ then wait
 
 class AstronomyTests: XCTestCase {
 
-    func testForSearchResults() {
+    func testFetchRover() {
+        func testFetchMarsRover() {
+              let client = MarsRoverClient()
+              let expectation = self.expectation(description: "Waiting for results")
+               client.fetchMarsRover(named: "Curiosity") { (rover, error) in
+                 expectation.fulfill()
+              }
+               wait(for: [expectation], timeout: 5)
+           }
+    }
+    func testValidData() {
         
-        let expectation = self.expectation(description: "Wait for results")
-        
+        let expectation = self.expectation(description: "Waiting for data")
+        let roverClient = MarsRoverClient()
+
+         roverClient.fetchMarsRover(named: "Curiosity") { data, error in
+            guard let data = data else {
+                XCTFail("Failed to get data")
+                return
+            }
+            XCTAssertNotNil(data)
+            expectation.fulfill()
+        }
+         wait(for: [expectation], timeout: 5)
+    }
+    func testPhotosFetch() {
+        let rover = MarsRover(name: "Curiosity",
+                                    launchDate: Date(),
+                                    landingDate: Date(),
+                                    status: .active,
+                                    maxSol: 10,
+                                    maxDate: Date(),
+                                    numberOfPhotos: 4156,
+                                    solDescriptions: [SolDescription(sol: 3, totalPhotos: 338, cameras: ["MAST"])])
+        let expectation = self.expectation(description: "Waiting for Photos")
+        let roverClient = MarsRoverClient()
+         roverClient.fetchPhotos(from: rover, onSol: 3) { data, error in
+            guard let data = data else {
+                XCTFail("Failed to get photos for Curiosity")
+                return
+            }
+            XCTAssertNotNil(data)
+            XCTAssertEqual(data.count, 338)
+            expectation.fulfill()
+        }
+
+         wait(for: [expectation], timeout: 10)
+    }
+    func testJSON() {
+            let expectation = self.expectation(description: "TestingJSON")
+            let dataLoader = MockDataLoader(data: validRoverJSON, error: nil)
+            let roverClient = MarsRoverClient(dataLoader: dataLoader)
+
+             roverClient.fetchMarsRover(named: "Curiosity") { rover, error in
+                XCTAssertNoThrow(rover)
+
+                 if let rover = rover {
+                    XCTAssertTrue(rover.status == .active)
+                    XCTAssertGreaterThan(rover.maxSol, 9)
+                    XCTAssertEqual(rover.numberOfPhotos, 4156)
+                }
+                expectation.fulfill()
+            }
+
+             wait(for: [expectation], timeout: 5)
+        }
+    func testInvalidJSON() {
+        let expectation = self.expectation(description: "Testing for invalid JSON")
+        let dataLoader = MockDataLoader(data: invalidJSON, error: nil)
+        let roverClient = MarsRoverClient(dataLoader: dataLoader)
+
+         roverClient.fetchMarsRover(named: "Curiosity") { rover, error in
+            XCTAssertNil(rover)
+            XCTAssertNotNil(error)
+            expectation.fulfill()
+        }
+
+         wait(for: [expectation], timeout: 5)
     }
     
 }
--- Mocking/MockDataLoading.swift
+++ Mocking/MockDataLoading.swift
@@ -10,26 +10,24 @@ import Foundation
 @testable import Astronomy
 
 class MockDataLoader: NetworkDataLoader {
-  
+    
     let data: Data?
     let error: Error?
     
     internal init(data: Data?, error: Error?) {
-          self.data = data
-          self.error = error
-      }
+        self.data = data
+        self.error = error
+    }
     
     func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
-       DispatchQueue.main.asyncAfter(deadline: .now() + 0.005) {
-                 completion(self.data, self.error)
-             }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.005) {
+            completion(self.data, self.error)
+        }
     }
     
     func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
-          DispatchQueue.main.asyncAfter(deadline: .now() + 0.005) {
-                  completion(self.data, self.error)
-              }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.005) {
+            completion(self.data, self.error)
+        }
     }
-    
-    
 }
