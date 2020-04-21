809968ee8a91c74dbe0bfc4985f152145d1b53c8
Ezra Black <dev.ezrablack@gmail.com>
2020-04-20 19:58:57 -0500

    Added dataLoader to the client prepared first expectation implemented NetworkDataLoader URLSession NetworkDataLoader extension

--- AstronomyTests/AstronomyTests.swift
+++ AstronomyTests/AstronomyTests.swift
@@ -26,24 +26,10 @@ then wait
 
 class AstronomyTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testForSearchResults() {
+        
+        let expectation = self.expectation(description: "Wait for results")
+        
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
