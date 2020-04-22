05f56225173cbef4627dac872b2a6d47768d5e1d
David Wright <davidtwright89@gmail.com>
2020-04-22 00:09:56 -0400

    Added a unit test target and created skeleton test cases

---
+++ AstronomyTests/AstronomyTests.swift
@@ -0,0 +1,29 @@
+//
+//  AstronomyTests.swift
+//  AstronomyTests
+//
+//  Created by David Wright on 4/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class AstronomyTests: XCTestCase {
+
+    func testFailedFetchMarsRover() {
+        
+    }
+    
+    func testFetchMarsRover() {
+        
+    }
+
+    func testFetchPhotos() {
+        
+    }
+    
+    func testFetch() {
+        
+    }
+
+}
