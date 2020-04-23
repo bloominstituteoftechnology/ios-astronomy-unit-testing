beb3b386658cc80f4cca96423691f4fd0da042e3
Jessie <jessie.griffin@icloud.com>
2020-04-22 22:30:51 -0400

    Changed Sol to test something

--- Astronomy/Networking/MarsRoverClient.swift
+++ Astronomy/Networking/MarsRoverClient.swift
@@ -22,7 +22,6 @@ class MarsRoverClient {
         
         let url = self.url(forInfoForRover: name)
         fetch(from: url, using: session) { (dictionary: [String : MarsRover]?, error: Error?) in
-
             guard let rover = dictionary?["photo_manifest"] else {
                 completion(nil, error)
                 return
