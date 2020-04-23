ad9e7136744490340f80fcc8560201551309b4c0
Sal <salmanamer@gmail.com>
2020-04-22 19:17:21 -0700

    setup UI Test

---
+++ Astronomy/Utilities/NetworkDataLoader.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkDataLoader.swift
+//  Astronomy
+//
+//  Created by Sal B Amer on 4/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ AstronomyTests/AstronomyTests.swift
@@ -0,0 +1,28 @@
+//
+//  AstronomyTests.swift
+//  AstronomyTests
+//
+//  Created by Sal B Amer on 4/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+@testable import Astronomy
+
+/* test the following
+ 
+ Does Decoding Work?
+ Does Decoding fail when given bad data?
+ does it build the correct URL?
+ Does it build the corect URLRequest? Get vs Post?
+ Are the search results saved properly?
+ Is the completion handler called when data is bad?
+ Is the completion handler called when networking failed?
+ */
+
+
+class AstronomyTests: XCTestCase {
+
+   
+
+}
