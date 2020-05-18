73c4ded8e6b235576f1356bee7d98ec0106f9d55
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-18 14:21:34 -0700

    Fetching MarsRover test succeed in fetching.

--- AstronomyTests/AstronomyTests.swift
+++ AstronomyTests/AstronomyTests.swift
@@ -7,27 +7,22 @@
 //
 
 import XCTest
-
+@testable import Astronomy
 class AstronomyTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
+ 
+    func testFetchMarsRover() {
         measure {
-            // Put the code you want to measure the time of here.
+        let expectation = self.expectation(description: "Waiting for results")
+        let roverController = MarsRoverClient()
+        
+        roverController.fetchMarsRover(named: "curiosity") { (marsrover, error) in
+            expectation.fulfill()
         }
+            wait(for: [expectation], timeout: 5)
     }
+    }
+    
+    
 
 }
