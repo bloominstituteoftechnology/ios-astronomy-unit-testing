eba877e0e23fe1e344009edece2051be6e49824b
Chris Dobek <dobek.christopher@gmail.com>
2020-05-18 16:05:34 -0400

    Implemented both loadData funcs in MockDataLoader

--- AstronomyTests/MockDataLoader.swift
+++ AstronomyTests/MockDataLoader.swift
@@ -10,4 +10,27 @@ import Foundation
 @testable import Astronomy
 
 class MockDataLoader: NetworkDataLoader {
+    
+    // MARK: - Properties
+    let data: Data?
+    let error: Error?
+    
+    internal init(data: Data?, error: Error?) {
+        self.data = data
+        self.error = error
+    }
+    
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 0.05) {
+            completion(self.data, self.error)
+        }
+    }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 0.05) {
+            completion(self.data, self.error)
+        }
+    }
+    
+    
 }
