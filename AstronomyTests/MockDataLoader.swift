1d5884373840dedbbaf172cf453d279a78afbe58
Chris Dobek <dobek.christopher@gmail.com>
2020-05-18 15:59:17 -0400

    Created MockDataLoader file

---
+++ AstronomyTests/MockDataLoader.swift
@@ -0,0 +1,13 @@
+//
+//  MockDataLoader.swift
+//  AstronomyTests
+//
+//  Created by Chris Dobek on 5/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+@testable import Astronomy
+
+class MockDataLoader: NetworkDataLoader {
+}
