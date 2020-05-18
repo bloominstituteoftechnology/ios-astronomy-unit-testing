7133366e4640a3b1976ddf8643f2a608698764a9
Chris Dobek <dobek.christopher@gmail.com>
2020-05-18 15:26:13 -0400

    Wrote NetworkDataLoader protocol

--- Astronomy/Model Controller/NetworkDataLoader.swift
+++ Astronomy/Model Controller/NetworkDataLoader.swift
@@ -7,3 +7,10 @@
 //
 
 import Foundation
+
+protocol NetworkDataLoader {
+    
+    func loadData(from request: URLRequest, completion: @escaping( Data?, Error?) -> Void)
+    
+    func loadData(from url: URL, completion: @escaping( Data?, Error?) -> Void)
+}
