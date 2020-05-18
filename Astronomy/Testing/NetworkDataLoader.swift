556937880b524b6f811ca55122024d2717e87fc1
Cameron Collins <cameroncollins19@yahoo.com>
2020-05-18 14:18:21 -0500

    Added init and networkLoader in MarsRoverClient

--- Astronomy/Testing/NetworkDataLoader.swift
+++ Astronomy/Testing/NetworkDataLoader.swift
@@ -17,11 +17,11 @@ protocol NetworkDataLoader {
 extension URLSession: NetworkDataLoader {
     
     func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
-        <#code#>
+        //TODO
     }
     
     func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
-        <#code#>
+        //TODO
     }
     
     
