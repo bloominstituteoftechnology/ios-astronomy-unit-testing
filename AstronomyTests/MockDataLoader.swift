5aa667eb3153143075f10e27bd89e0ab7b621dcb
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-18 15:21:35 -0700

    added methods to the mock data loader.

--- AstronomyTests/MockDataLoader.swift
+++ AstronomyTests/MockDataLoader.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+@testable import Astronomy
+
+struct MockDataLoader: NetworkDataLoader {
+  
+    var data: Data?
+       var error: Error?
+    
+    internal init(data: Data? = nil, error: Error? = nil) {
+        self.data = data
+        self.error = error
+    }
+    
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+          DispatchQueue.main.asyncAfter(deadline: .now() + 0.005 /* ms */) {
+              completion(self.data, self.error)
+          }
+      }
+      
+      func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+          DispatchQueue.main.asyncAfter(deadline: .now() + 0.005 /* ms */) {
+              completion(self.data, self.error)
+          }
+      }
+   
+}
+
