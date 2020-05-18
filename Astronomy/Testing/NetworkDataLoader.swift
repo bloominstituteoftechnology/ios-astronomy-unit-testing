730ff95e7764bef848f11be65888d4be5fa01d32
Cameron Collins <cameroncollins19@yahoo.com>
2020-05-18 14:07:07 -0500

    Added NetworkDataLoader File

---
+++ Astronomy/Testing/NetworkDataLoader.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkDataLoader.swift
+//  Astronomy
+//
+//  Created by Cameron Collins on 5/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkDataLoader {
+    
+}
