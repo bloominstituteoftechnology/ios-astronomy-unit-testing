eab37cff46a4072c9bfea00dc92e37318cb08225
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-18 15:26:00 -0700

    MarsRoverData tested successful

--- AstronomyTests/AstronomyTests.swift
+++ AstronomyTests/AstronomyTests.swift
@@ -55,6 +55,24 @@ class AstronomyTests: XCTestCase {
         }
         
     }
+    func testValidMarsRoverData() {
+        let expectation = self.expectation(description: "Wait for results")
+        let mockDataLoader = MockDataLoader(data: validRoverJSON, error: nil)
+
+        let client = MarsRoverClient(networkLoader: mockDataLoader)
+
+        client.fetchMarsRover(named: "curiosity") { (rover, error) in
+            XCTAssertNotNil(rover, "Rover object is nil")
+
+            if let rover = rover {
+                XCTAssertEqual(rover.solDescriptions[4].sol, 10, "sol is unexpected")
+            }
+
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 5)
+    }
     
     
 }
