a75bf5c5244ab143007422192d35269f9d6022da
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-18 12:44:47 -0700

    Added the protocol stubs to the URLSession extension from NetworkData

--- Astronomy/Model/NetworkDataLoader.swift
+++ Astronomy/Model/NetworkDataLoader.swift
@@ -14,3 +14,20 @@ protocol NetworkDataLoader {
     
     func loadData(from url: URL, completion: @escaping( Data?, Error?) -> Void)
 }
+
+extension URLSession: NetworkDataLoader {
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        dataTask(with: request) { (data, _, error) in
+            completion(data, error)
+        }.resume()
+    }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        dataTask(with: url) { (data, _, error) in
+            completion(data, error)
+        }.resume()
+    }
+    
+    
+   
+}
