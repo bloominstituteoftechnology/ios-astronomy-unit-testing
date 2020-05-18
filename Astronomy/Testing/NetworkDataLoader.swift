1fcd1f8af9959f0d518751a6ee5c43bf8ca29e74
Cameron Collins <cameroncollins19@yahoo.com>
2020-05-18 14:10:06 -0500

    Added NetworkDataLoader protocol and extension for URLSession

--- Astronomy/Testing/NetworkDataLoader.swift
+++ Astronomy/Testing/NetworkDataLoader.swift
@@ -9,5 +9,20 @@
 import Foundation
 
 protocol NetworkDataLoader {
+    func loadData(from request: URLRequest, completion: @escaping(Data?, Error?) -> Void)
+    
+    func loadData(from url: URL, completion: @escaping(Data?, Error?) -> Void)
+}
+
+extension URLSession: NetworkDataLoader {
+    
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        <#code#>
+    }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        <#code#>
+    }
+    
     
 }
