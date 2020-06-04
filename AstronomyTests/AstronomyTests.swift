c65bee759977f70d2ba2c2208ff23e55317bec6d
Matt Martindale <martindalematt18@yahoo.com>
2020-06-03 19:39:33 -0600

    test for fetchingRover photos successful

--- AstronomyTests/AstronomyTests.swift
+++ AstronomyTests/AstronomyTests.swift
@@ -18,15 +18,43 @@ class AstronomyTests: XCTestCase {
         let mockLoader = MockLoader(data: testJSON.validRoverJSON, error: nil)
         let marsRoverClient = MarsRoverClient(networkDataLoader: mockLoader)
         
-        marsRoverClient.fetchMarsRover(named: "Tim") { (possibleData, possibleError) in
+        marsRoverClient.fetchMarsRover(named: "Curiosity") { (rover, possibleError) in
             XCTAssertNil(possibleError)
-            XCTAssertNotNil(possibleData)
+            XCTAssert(rover?.name == "Curiosity")
             expectation.fulfill()
         }
         wait(for: [expectation], timeout: 5)
     }
     
-    
+    func testAnother() {
+        let testJSON = TestJSON()
+        let mockLoader = MockLoader(data: testJSON.validRoverJSON, error: nil)
+        let marsRoverClient = MarsRoverClient(networkDataLoader: mockLoader)
+        
+        let marsRoverExpectation = expectation(description: "Waiting to fetch Mars rover")
+        var curiosityRover: MarsRover?
+        
+        // Fetch mars rover Curiosity
+        marsRoverClient.fetchMarsRover(named: "Curiosity") { (possibleRover, possibleError) in
+            if let rover = possibleRover {
+                curiosityRover = rover
+            }
+            marsRoverExpectation.fulfill()
+        }
+        wait(for: [marsRoverExpectation], timeout: 5)
+        XCTAssert(curiosityRover != nil)
+        XCTAssert(curiosityRover?.name == "Curiosity")
+        
+        let fetchPhotosExpectation = expectation(description: "Waiting to fetch Curiosity photos")
+        // Using fetched Curiosity rover, fetch photos from rover
+        guard let rover = curiosityRover else { print("No Rover"); return }
+        marsRoverClient.fetchPhotos(from: rover, onSol: 2) { (possiblePhotos, possibleError) in
+            XCTAssertNil(possibleError)
+            XCTAssertNotNil(possiblePhotos)
+            fetchPhotosExpectation.fulfill()
+        }
+        wait(for: [fetchPhotosExpectation], timeout: 5)
+    }
  
     
 }
