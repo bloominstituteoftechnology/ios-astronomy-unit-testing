41b83bf0595a3b0367df5fc3efaa3b4294980bbb
Claudia Maciel <clc_80@yahoo.com>
2020-06-03 20:01:00 -0700

    Added test to test fetching of data

--- AstronomyTests/AstronomyTests.swift
+++ AstronomyTests/AstronomyTests.swift
@@ -7,27 +7,29 @@
 //
 
 import XCTest
+@testable import Astronomy
 
 class AstronomyTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testMarsRoverFetch() {
+        let mockSession = MockLoader(data: validRoverJSON, error: nil)
+        let marsRoverClient = MarsRoverClient(networkLoader: mockSession)
+        let expectation = self.expectation(description: "Waiting for Rover Data")
+        
+        marsRoverClient.fetchMarsRover(named: "Curiosity") { (marsRover, error) in
+            XCTAssertNil(error, "Expected error = nil, but got error: \(error!)")
+            guard let marsRover = marsRover else {
+                XCTFail("Mars Rover data unexpectedly equals nil.")
+                return
+            }
+            XCTAssertEqual(marsRover.name, "Curiosity")
+            XCTAssertEqual(marsRover.numberOfPhotos, 4156)
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+            expectation.fulfill()
         }
+
+        wait(for: [expectation], timeout: 5)
+        
     }
 
 }
