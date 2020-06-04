d90027e3b3db7c44f6cbbcb910ab88e4b759ff8a
Libby Thomas <thomaslibby05@gmail.com>
2020-06-03 21:33:31 -0500

    MockURLSession created

--- Astronomy/Model/NetworkDataLoader.swift
+++ Astronomy/Model/NetworkDataLoader.swift
@@ -28,4 +28,27 @@ extension URLSession : NetworkDataLoader {
     }
 }
 
+class MockURLSession : NetworkDataLoader {
+    
+    let data: Data?
+    let error: Error?
+    
+    init(data: Data?, error: Error?) {
+        self.data = data
+        self.error = error
+    }
+    
+    func loadData(from request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        DispatchQueue.global().async {
+            completion(self.data, nil, self.error)
+        }
+    }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        DispatchQueue.global().async {
+            completion(self.data, nil, self.error)
+        }
+    }
+}
+
 
---
+++ MarsRoverClientTests/MarsRoverClientTests.swift
@@ -0,0 +1,22 @@
+//
+//  MarsRoverClientTests.swift
+//  MarsRoverClientTests
+//
+//  Created by Elizabeth Thomas on 6/3/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+@testable import Astronomy
+
+class MarsRoverClientTests: XCTestCase {
+
+    func fetchMarsRover() {
+        
+    }
+    
+    func fetchPhotos() {
+        
+    }
+    
+}
