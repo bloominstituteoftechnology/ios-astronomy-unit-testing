936968e13fea6704543307cdbf2e278bd61a1281
Thomas Dye <thomasallendye@gmail.com>
2020-06-03 22:17:29 -0400

    Added the networkLoader property. Added init for MarsRoverClient

--- Astronomy/Networking/NetworkDataLoader.swift
+++ Astronomy/Networking/NetworkDataLoader.swift
@@ -12,7 +12,6 @@ protocol NetworkDataLoader {
     
     func loadData(from request: URLRequest,
                   completion: @escaping (Data?, Error?) -> Void)
-    
     func loadData(from url: URL,
                   completion: @escaping (Data?, Error?) -> Void)
 }
@@ -21,17 +20,20 @@ extension URLSession: NetworkDataLoader {
     
     func loadData(from request: URLRequest,
                   completion: @escaping (Data?, Error?) -> Void) {
-        let dataTask = URLSession.shared.dataTask(with: request) { (data, _, error) in
+        
+        let dataTask = self.dataTask(with: request) { (data, _, error) in
             completion(data, error)
         }
         dataTask.resume()
     }
-
+    
     func loadData(from url: URL,
                   completion: @escaping (Data?, Error?) -> Void) {
-        let dataTask = URLSession.shared.dataTask(with: url) { (data, _, error) in
+        
+        let dataTask = self.dataTask(with: url) { (data, _, error) in
             completion(data, error)
         }
+        
         dataTask.resume()
     }
 }
