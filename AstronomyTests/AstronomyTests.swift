93b90aee29f623041a2a18a1d6047601209272cd
Thomas Dye <thomasallendye@gmail.com>
2020-06-03 22:47:44 -0400

    Created testRoverFetchPassed, testRoverFetchFailed, and testFetchingPhotos tests.

--- AstronomyTests/AstronomyTests.swift
+++ AstronomyTests/AstronomyTests.swift
@@ -6,28 +6,93 @@
 //  Copyright © 2020 Lambda School. All rights reserved.
 //
 
+import Foundation
 import XCTest
 
-class AstronomyTests: XCTestCase {
+@testable import Astronomy
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+class MarsRoverClientTests: XCTestCase {
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    struct MockLoader: NetworkDataLoader {
+        
+        var data: Data?
+        var error: Error?
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+        func loadData(from request: URLRequest,
+                      completion: @escaping (Data?, Error?) -> Void) {
+            
+            DispatchQueue.global().async {
+                completion(self.data, self.error)
+            }
+        }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+        func loadData(from url: URL,
+                      completion: @escaping (Data?, Error?) -> Void) {
+            
+            DispatchQueue.global().async {
+                completion(self.data, self.error)
+            }
         }
     }
 
+    func testRoverFetchPassed() {
+        
+        let mockLoader = MockLoader(data: validRoverJSON)
+        let client = MarsRoverClient(loader: mockLoader)
+        let expectedResult = expectation(description: "fetching")
+        var marsRover: MarsRover?
+        
+        client.fetchMarsRover(named: "Curiosity") { (rover, error) in
+            expectedResult.fulfill()
+            marsRover = rover
+        }
+
+        wait(for: [expectedResult],
+             timeout: 5)
+        XCTAssert(marsRover?.name != nil)
+        XCTAssertEqual("Curiosity", marsRover?.name)
+        XCTAssert((marsRover?.solDescriptions.count)! > 0)
+    }
+
+    func testRoverFetchFailed() {
+        
+        let mockLoader = MockLoader(data: inValidRoverJSON)
+        let client = MarsRoverClient(loader: mockLoader)
+        let expectedResult = expectation(description: "fetching")
+        var marsRover: MarsRover?
+        
+        client.fetchMarsRover(named: "Curiosity") { (rover, error) in
+            expectedResult.fulfill()
+            marsRover = rover
+        }
+
+        wait(for: [expectedResult],
+             timeout: 5)
+        XCTAssert(marsRover?.name == nil)
+        XCTAssert(marsRover?.solDescriptions.count == nil)
+    }
+
+    func testFetchingPhotos() {
+        
+        let mockLoader = MockLoader(data: validSol1JSON)
+        let client = MarsRoverClient(loader: mockLoader)
+        let expectedResult = expectation(description: "loading photos")
+        let rover = MarsRover(name: "TestRover",
+                              launchDate: Date(),
+                              landingDate: Date(),
+                              status: .active,
+                              maxSol: 0,
+                              maxDate: Date(),
+                              numberOfPhotos: 2,
+                              solDescriptions: [])
+        
+        client.fetchPhotos(from: rover,
+                           onSol: 3) { (photos, _) in
+                            
+                            expectedResult.fulfill()
+                            XCTAssert(photos?.isEmpty == false)
+        }
+
+        wait(for: [expectedResult], timeout: 5)
+    }
 }
