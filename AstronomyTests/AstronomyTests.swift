eabc960d75d1089bfe07dab23f6313cf048ed1f7
Chad Parker <chad@chadparker.com>
2020-06-03 19:27:34 -0700

    Add Unit Tests target

---
+++ AstronomyTests/AstronomyTests.swift
@@ -0,0 +1,26 @@
+//
+//  AstronomyTests.swift
+//  AstronomyTests
+//
+//  Created by Chad Parker on 6/3/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class AstronomyTests: XCTestCase {
+   
+   let app = XCUIApplication()
+   
+   override func setUpWithError() throws {
+      
+   }
+   
+   override func tearDownWithError() throws {
+      
+   }
+   
+   func testExample() throws {
+      
+   }
+}
