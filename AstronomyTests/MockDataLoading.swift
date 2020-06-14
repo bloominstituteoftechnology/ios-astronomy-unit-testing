d7706837307091b2d1263833f32b7606cb7e64b7
Kelson Hartle <kelson99hartle@icloud.com>
2020-06-14 16:29:35 -0600

    Created Mock Data loader file with data and error properties, as well as an initializer and conformed to the NetworkdataLoader protocol

--- AstronomyTests/MockDataLoading.swift
+++ AstronomyTests/MockDataLoading.swift
@@ -12,20 +12,23 @@ import Foundation
 class MockDataLoader: NetworkDataLoader { //this will do the work of a url session without actually having a network (internet) url session.
     
     let data: Data?
-    let response: URLResponse?
     let error: Error?
     
-    internal init(data: Data?, response: URLResponse?, error: Error?) {
+    internal init(data: Data?, error: Error?) {
         self.data = data
-        self.response = response
         self.error = error
     }
     
-    func loadData(using request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.005) { //this is not recomended in a general sense as a you can not stop it but a timer you can stop
-            completion(self.data, self.response, self.error)
-            
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.0005) {
+            completion(self.data, self.error)
         }
     }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.0005) {
+            completion(self.data, self.error)
+        }
+    }
+    
 }
