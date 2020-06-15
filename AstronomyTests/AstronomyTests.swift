1f8b9b2372c60ba3f6b6e4cff0b59d4e8fc91ade
Stephanie <user.name>
2020-06-15 02:15:24 -0700

    set up test functions basics and json mock data

--- AstronomyTests/AstronomyTests.swift
+++ AstronomyTests/AstronomyTests.swift
@@ -32,4 +32,89 @@ class AstronomyTests: XCTestCase {
         
     }
 
+    func testForSomeResults() {
+        
+        // TODO: fix
+//        let expectation = self.expectation(description: "Wait for results")
+//        let controller = MarsRoverClient()
+//        controller.fetchMarsRover(named: "Mast") {_,_ in
+//            print("We got back some results!")
+//            XCTAssertGreaterThan(controller., 0)
+//            expectation.fulfill()
+//        }
+//        wait(for: [expectation], timeout: 5)
+//        XCTAssertGreaterThan(controller., 0)
+    }
+    
+    func testSpeedOfTypicalRequest() {
+//        measure {
+//            let expectation = self.expectation(description: "Wait for results")
+//            let controller = MarsRoverClient(dataLoader: URLSession(configuration: .ephemeral))
+//
+//            controller.fetchPhotos(from: "Mast", onSol: 1) {
+//                expectation.fulfill()
+//            }
+//
+//            wait(for: [expectation], timeout:  5)
+//        }
+    }
+    
+    func testSpeedOfTypicalRequestMoreAccurately() {
+//        measureMetrics([.wallClockTime], automaticallyStartMeasuring: false) {
+//            let expectation = self.expectation(description: "Wait for results")
+//            let controller = MarsRoverClient(dataLoader: URLSession(configuration: .ephemeral))
+//
+//            startMeasuring()
+//            controller.fetchPhotos(from: "Mast", onSol: 1) {
+//                self.stopMeasuring()
+//                expectation.fulfill()
+//            }
+//            wait(for: [expectation], timeout: 5)
+//        }
+    }
+    
+    func testValidRoverData() {
+        let mockDataLoader = MockDataLoader(data: validRoverJSON, error: nil)
+        
+        let expectation = self.expectation(description: "Wait for results")
+        let controller = MarsRoverClient(dataLoader: mockDataLoader)
+        
+        controller
+        
+        
+    }
+    
+    func testInvalidRoverData() {
+        let mockDataLoader = MockDataLoader(data: invalidRoverJSON, error: nil)
+        
+        let expectation = self.expectation(description: "Wait for results")
+        let controller = MarsRoverClient(dataLoader: mockDataLoader)
+        
+        controller
+    }
+    
+    func testValidSolData() {
+        let mockDataLoader = MockDataLoader(data: validSol1JSON, error: nil)
+        
+        let expectation = self.expectation(description: "Wait for results")
+        let controller = MarsRoverClient(dataLoader: mockDataLoader)
+        
+        controller
+    }
+    
+    func testInvalidSolData() {
+        let mockDataLoader = MockDataLoader(data: invalidSol1JSON, error: nil)
+        
+        let expectation = self.expectation(description: "Wait for results")
+        let controller = MarsRoverClient(dataLoader: mockDataLoader)
+        
+        controller
+    }
+    
+    func testNoResultsData() {
+        let mockDataLoader = MockDataLoader(data: noResultsData, error: nil)
+        
+        let expectation = self.expectation(description: "Wait for results")
+        let controller = MarsRoverClient(dataLoader: mockDataLoader)
+    }
 }
