3de54989e82e99d83849ae26fb5f738494c500bc
Cody Morley <codymorleyinc@gmail.com>
2020-06-15 14:55:14 -0700

    finished mock data loader

---
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -0,0 +1,19 @@
+//
+//  AstronomyNetworkingTests.swift
+//  AstronomyTests
+//
+//  Created by Cody Morley on 6/15/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class MarsRoverClientTests: XCTestCase {
+    testFetchMarsRover() {
+        
+    }
+    
+    testFetchPhoto() {
+        
+    }
+}
