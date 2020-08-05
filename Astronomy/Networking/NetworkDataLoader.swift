8154c51f4c4217d7d00fcc57195c636fd3606b51
Clayton Watkins <watkinclayton17@yahoo.com>
2020-08-05 12:16:50 -0700

    Created Protocol for NetoworkDataLoader'

---
+++ Astronomy/Networking/NetworkDataLoader.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkDataLoader.swift
+//  Astronomy
+//
+//  Created by Clayton Watkins on 8/5/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkDataLoader {
+    
+    func loadData(from request: URLRequest, completion: @escaping( Data?, Error?) -> Void)
+    
+    func loadData(from url: URL, completion: @escaping( Data?, Error?) -> Void)
+    
+}
