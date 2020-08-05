40ae4dbb149a5e5868b17b3a038bacf2ade45bbd
Clayton Watkins <watkinclayton17@yahoo.com>
2020-08-05 12:14:41 -0700

    Esatablished Project and created Unit Test folder

---
+++ AstronomyTests/AstronomyTests.swift
@@ -0,0 +1,19 @@
+//
+//  AstronomyTests.swift
+//  AstronomyTests
+//
+//  Created by Clayton Watkins on 8/5/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+@testable import Astronomy
+
+class AstronomyTests: XCTestCase {
+
+    func testMarsRoverClient() {
+        
+    }
+
+
+}
