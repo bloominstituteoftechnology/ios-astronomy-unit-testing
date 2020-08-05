9890b3bfecdf31bd6c847560a3464b7c54bbe0d6
Libby Thomas <thomaslibby05@gmail.com>
2020-08-05 15:53:56 -0500

    Implemented NetworkDataLoader in MarsRoverClient file

--- Astronomy/Networking/MarsRoverClient.swift
+++ Astronomy/Networking/MarsRoverClient.swift
@@ -10,6 +10,12 @@ import Foundation
 
 class MarsRoverClient {
     
+    var networkLoader: NetworkDataLoader
+    
+    init(networkLoader: NetworkDataLoader = URLSession.shared) {
+        self.networkLoader = networkLoader
+    }
+    
     func fetchMarsRover(named name: String,
                         using session: URLSession = URLSession.shared,
                         completion: @escaping (MarsRover?, Error?) -> Void) {
@@ -43,9 +49,11 @@ class MarsRoverClient {
     // MARK: - Private
     
     private func fetch<T: Codable>(from url: URL,
-                           using session: URLSession = URLSession.shared,
+                           using session: NetworkDataLoader = URLSession.shared,
                            completion: @escaping (T?, Error?) -> Void) {
-        session.dataTask(with: url) { (data, response, error) in
+        
+        session.loadData(from: url) { (data, error) in
+        
             if let error = error {
                 completion(nil, error)
                 return
@@ -63,7 +71,8 @@ class MarsRoverClient {
             } catch {
                 completion(nil, error)
             }
-        }.resume()
+        }
+        
     }
     
     private let baseURL = URL(string: "https://api.nasa.gov/mars-photos/api/v1")!
