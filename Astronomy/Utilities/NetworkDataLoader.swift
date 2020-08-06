9f5ac930bfdbb7eced04b36e1b17ef1a8c24437a
davidWilliams96080 <burmesedragon@att.net>
2020-08-05 18:41:42 -0700

    Initial commit + successful implementation of network helper files/protocol

---
+++ Astronomy/Utilities/NetworkDataLoader.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkDataLoader.swift
+//  Astronomy
+//
+//  Created by David Williams on 8/5/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkDataLoader {
+    
+    func loadData(from request: URLRequest, completion: @escaping( Data?, Error?) -> Void)
+    
+    func loadData(from url: URL, completion: @escaping( Data?, Error?) -> Void)
+}
