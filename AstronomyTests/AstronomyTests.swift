5d4d0456348dcdee342bc676dcd797053b0fd99e
morgan smith <mssmitty07@gmail.com>
2020-08-06 17:13:32 -0400

    finished part 1, preparing for the tests

---
+++ AstronomyTests/AstronomyTests.swift
@@ -0,0 +1,14 @@
+//
+//  AstronomyTests.swift
+//  AstronomyTests
+//
+//  Created by Morgan Smith on 8/5/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class AstronomyTests: XCTestCase {
+
+
+}
