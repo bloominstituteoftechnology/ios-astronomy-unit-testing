f04bcef56132a06079f4b8421dcb79bf2757b110
morgan smith <mssmitty07@gmail.com>
2020-08-06 20:21:13 -0400

    trying to write test but cant figure out how to reference fetched rover data

--- AstronomyTests/AstronomyTests.swift
+++ AstronomyTests/AstronomyTests.swift
@@ -7,15 +7,36 @@
 //
 
 import XCTest
+@testable import Astronomy
 
 class AstronomyTests: XCTestCase {
 
-    func testFetchMarsRover() {
+    func testValidRoverData() {
+        let expectation = self.expectation(description: "Collection view should return info for rover")
 
+        // Mimicking the URLSessionDataTask, and assuming that we get data and no response or error.
+        let mockLoader = MockLoader(data: validRoverJSON, error: nil)
+
+        let marsRoverClient = MarsRoverClient(networkLoader: mockLoader)
+        let photosView = PhotosCollectionViewController()
+
+        marsRoverClient.fetchMarsRover(named: "Curiosity") {_,_ in
+
+            // This should fail
+            XCTAssertFalse()
+
+            print("Fulfilling expectation")
+            expectation.fulfill()
+
+        }
+
+        print("Waiting for expectation(s)")
+        waitForExpectations(timeout: 5)
+        print("Done waiting for expectations")
     }
 
     func testFetchPhotos() {
-        
+
     }
 
 
