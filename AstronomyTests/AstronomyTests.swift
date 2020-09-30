292b87236840f7557174e41cfad61be4b51d52ba
Gladymir <gaethano815@gmail.com>
2020-09-30 11:53:36 -0600

    Created Unit Test Folder

---
+++ AstronomyTests/AstronomyTests.swift
@@ -0,0 +1,17 @@
+//
+//  AstronomyTests.swift
+//  AstronomyTests
+//
+//  Created by Gladymir Philippe on 9/30/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+@testable import Astronomy
+
+class AstronomyTests: XCTestCase {
+
+    func testMarsRoverClient() {
+        
+    }
+}
