c5a6b9c8c2fc9c701a424a7430118ecdd8a35681
Kenneth Jones <kennethjones1991@outlook.com>
2020-09-30 21:19:47 -0600

    Completed Part 1 - Added NetworkDataLoader; added extension to URLSession; refactored MarsRoverClient

---
+++ Astronomy/Utilities/URLSession+NetworkDataLoader.swift
@@ -0,0 +1,25 @@
+//
+//  URLSession+NetworkDataLoader.swift
+//  Astronomy
+//
+//  Created by Kenneth Jones on 9/30/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+extension URLSession: NetworkDataLoader {
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        let task = self.dataTask(with: request) { (data, _, error) in
+            completion(data, error)
+        }
+        task.resume()
+    }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        let task = self.dataTask(with: url) { (data, _, error) in
+            completion(data, error)
+        }
+        task.resume()
+    }
+}
---
+++ AstronomyTests/AstronomyTests.swift
@@ -0,0 +1,17 @@
+//
+//  AstronomyTests.swift
+//  AstronomyTests
+//
+//  Created by Kenneth Jones on 9/30/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+@testable import Astronomy
+
+class AstronomyTests: XCTestCase {
+    
+    
+
+}
