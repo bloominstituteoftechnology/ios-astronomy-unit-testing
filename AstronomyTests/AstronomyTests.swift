651c5b2bdcb27f71b7f3f90633cf966ae4d5f0bc
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-09-30 19:07:38 -0700

    Set up project

---
+++ AstronomyTests/AstronomyTests.swift
@@ -0,0 +1,15 @@
+//
+//  AstronomyTests.swift
+//  AstronomyTests
+//
+//  Created by Cora Jacobson on 9/30/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class AstronomyTests: XCTestCase {
+
+    
+
+}
